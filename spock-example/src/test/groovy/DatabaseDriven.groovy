26f879911373b87942771b13dc5181790d16840b
Peter Niederwieser <pniederw@gmail.com>
2009-08-01 22:22:17 +0000

    set more version tags to 0.3-SNAPSHOT polishing

--- spock-example/src/test/groovy/DatabaseDriven.groovy
+++ spock-example/src/test/groovy/DatabaseDriven.groovy
@@ -9,7 +9,7 @@ import groovy.sql.Sql
 class DatabaseDriven {
   @Shared sql = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
   
-  // usually, the database would already contain the test data
+  // usually, the test data would already be contained in the database
   def setupSpeck() {
     sql.execute("create table maxdata (id int primary key, a int, b int, c int)")
     sql.execute("insert into maxdata values (1, 3, 7, 7), (2, 5, 4, 5), (3, 9, 9, 9)")

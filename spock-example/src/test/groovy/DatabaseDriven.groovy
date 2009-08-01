9e31c2b2173a0aaaf8f0a483b840f4f3b37f2fb8
Peter Niederwieser <pniederw@gmail.com>
2009-08-01 18:18:24 +0000

    polishing

--- spock-example/src/test/groovy/DatabaseDriven.groovy
+++ spock-example/src/test/groovy/DatabaseDriven.groovy
@@ -9,7 +9,7 @@ import groovy.sql.Sql
 class DatabaseDriven {
   @Shared sql = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
   
-  // usually we would have an external database with pre-loaded test data
+  // usually, the database would already contain the test data
   def setupSpeck() {
     sql.execute("create table maxdata (id int primary key, a int, b int, c int)")
     sql.execute("insert into maxdata values (1, 3, 7, 7), (2, 5, 4, 5), (3, 9, 9, 9)")

1dc25601c0891acbfa2c2c3bb5dadfbc63891d8b
Luke Daley <ld@ldaley.com>
2009-10-21 15:30:15 +0000

    Moved the stream swapping business out into a separate utility class.

--- spock-grails/src/groovy/grails/plugin/spock/build/GrailsSpeckRunner.groovy
+++ spock-grails/src/groovy/grails/plugin/spock/build/GrailsSpeckRunner.groovy
@@ -11,10 +11,6 @@ import org.apache.tools.ant.taskdefs.optional.junit.JUnitTest
 
 class GrailsSpeckRunner {
     
-    protected savedOut
-    protected savedErr
-    protected out
-    protected err
     protected formattedOutputs
 
     protected reportsDir
@@ -27,47 +23,37 @@ class GrailsSpeckRunner {
     
     def runTests(suite) {
         
+        def outAndErrSwapper = new SystemOutAndErrSwapper()
         def results = new JUnitResultsWrapper()
         def listener = new GrailsSpeckRunListener(System.out)
         def junit = new JUnitCore()
         junit.addListener(listener)
 
         suite.specks.each { speck ->
-            reset()
+            
             def junitTest = new JUnitTest(speck.name)
-            try {
-                replaceStandardStreams()
-                prepareReports(speck)
-                formattedOutputs.each { it.start(junitTest) }
+            outAndErrSwapper.swap { out, err ->
+                try {
+                    prepareReports(speck)
+                    formattedOutputs.each { it.start(junitTest) }
 
-                listener.setSpeck(speck, formattedOutputs) {
-                    def start = System.currentTimeMillis()
-                    def result = junit.run(speck)
-                    junitTest.runTime = System.currentTimeMillis() - start
-                    junitTest.setCounts(result.runCount, result.failureCount, 0)
+                    listener.setSpeck(speck, formattedOutputs) {
+                        def start = System.currentTimeMillis()
+                        def result = junit.run(speck)
+                        junitTest.runTime = System.currentTimeMillis() - start
+                        junitTest.setCounts(result.runCount, result.failureCount, 0)
 
-                    results << result
-                }
-            } catch (Throwable t) {
-                t.printStackTrace()
-            } finally {
-                formattedOutputs.each { output ->
-                    output.end(junitTest, out.toString(), err.toString())
-                }
-                
-                restoreStandardStreams()
+                        results << result
+                    }
+                } finally {
+                    formattedOutputs.each { output ->
+                        output.end(junitTest, out.toString(), err.toString())
+                    }
+                }    
             }
         }
 
         results
-    }
-
-    protected void reset() {
-        this.formattedOutputs = null
-        this.savedOut = null
-        this.savedErr = null
-        this.out = null
-        this.err = null
     }
     
     protected void prepareReports(Class speck) {
@@ -90,19 +76,4 @@ class GrailsSpeckRunner {
         }
     }
 
-    protected void replaceStandardStreams() {
-        this.savedOut = System.out
-        this.savedErr = System.err
-
-        this.out = new ByteArrayOutputStream()
-        this.err = new ByteArrayOutputStream()
-
-        System.setOut(new PrintStream(this.out))
-        System.setErr(new PrintStream(this.err))
-    }
-
-    protected void restoreStandardStreams() {
-        if (this.savedOut != null) System.out = this.savedOut
-        if (this.savedErr != null) System.err = this.savedErr
-    }
 }
---
+++ spock-grails/src/groovy/grails/plugin/spock/build/SystemOutAndErrSwapper.groovy
@@ -0,0 +1,47 @@
+package grails.plugin.spock.build
+
+// TODO this class needs a better name and method name
+class SystemOutAndErrSwapper {
+
+    protected swappedOutOut
+    protected swappedOutErr
+    
+    protected swappedInOut
+    protected swappedInErr
+    
+    def swap(Closure swappedFor) {
+        this.swappedOutOut = System.out
+        this.swappedOutErr = System.err
+
+        this.swappedInOut = new ByteArrayOutputStream()
+        this.swappedInErr = new ByteArrayOutputStream()
+
+        System.setOut(new PrintStream(this.swappedInOut))
+        System.setErr(new PrintStream(this.swappedInErr))
+        
+        def streams = [this.swappedInOut, this.swappedInErr]
+        
+        switch(swappedFor.maximumNumberOfParameters) {
+            case 0:
+                swappedFor()
+            break
+            case 1:
+                swappedFor(streams)
+            break
+            default:
+                swappedFor(*streams)
+            break
+        }
+        
+        System.out = this.swappedOutOut
+        System.err = this.swappedOutErr
+        
+        this.swappedOutOut = null
+        this.swappedOutErr = null
+
+        this.swappedInOut = null
+        this.swappedInErr = null
+        
+        streams
+    }
+}

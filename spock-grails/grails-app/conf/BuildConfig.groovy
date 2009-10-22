4aa99bf0e1f2e2f65a70b0b6e33796f1774ed8a6
Luke Daley <ld@ldaley.com>
2009-10-22 02:02:08 +0000

    Use the ‘build’ scope for all dependencies as it is more correct, and it still makes things available for tests.

--- spock-grails/grails-app/conf/BuildConfig.groovy
+++ spock-grails/grails-app/conf/BuildConfig.groovy
@@ -41,8 +41,8 @@ grails.project.dependency.resolution = {
     // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
     // runtime 'com.mysql:mysql-connector-java:5.1.5'
 
-    test 'org.spockframework:spock-core:0.3-SNAPSHOT'
-    test 'asm:asm:2.2.3'
+    build 'org.spockframework:spock-core:0.3-SNAPSHOT'
+    build 'asm:asm:2.2.3'
     build 'junit:junit:4.7'
   }
 }

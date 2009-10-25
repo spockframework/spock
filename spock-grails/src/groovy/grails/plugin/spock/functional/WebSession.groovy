fc3b7ac26e0bb2a0bc213ba6297839feb6257593
Luke Daley <ld@ldaley.com>
2009-10-25 12:40:14 +0000

    More javadoc.

--- spock-grails/src/groovy/grails/plugin/spock/functional/WebSession.groovy
+++ spock-grails/src/groovy/grails/plugin/spock/functional/WebSession.groovy
@@ -114,38 +114,83 @@ class WebSession {
     }
   }
   
+  /**
+   * @return the current window of the client
+   * @see WebClient#getCurrentWindow()
+   */
   WebWindow getCurrentWindow() {
     client.currentWindow
   }
-  
+
+  /**
+   * @return the enclosed page of the current window
+   * @see WebWindow#getEnclosedPage()
+   */  
   Page getPage() {
     currentWindow?.enclosedPage
   }
 
+  /**
+   * @return the response associated with the current page
+   * @see Page#getWebResponse()
+   */  
   WebResponse getResponse() {
     page?.webResponse
   }
-  
+
+  /**
+   * @return the request settings used to generate the current response
+   * @see WebResponse#getRequestSettings()
+   */    
   WebRequestSettings getRequestSettings() {
     response?.requestSettings
   }
-  
+
+  /**
+   * @return the URL of the current request
+   * @see WebRequestSettings#getURL()
+   */
   URL getRequestURL() {
     requestSettings.url
   }
   
+  /**
+   * Should the client automatically follow redirects?
+   * 
+   * @see WebClient#setRedirectEnabled()
+   */
   void setRedirectEnabled(boolean flag) {
     client.redirectEnabled = flag
   }
-  
+
+  /**
+   * Does the client automatically follow redirects?
+   * 
+   * @see WebClient#isRedirectEnabled()
+   */  
   boolean isRedirectEnabled() {
     client.redirectEnabled
   }
   
+  /**
+   * Is the current response a redirect?
+   * 
+   * @see isRedirectStatus(int)
+   * @return true, if the response was a redirect, false if it wasn't or no request has been made
+   */
   boolean isDidReceiveRedirect() {
     (page) ? isRedirectStatus(response.statusCode) : false
   }
-  
+
+  /**
+   * If the previous request resulted in a redirect, follow it.
+   * 
+   * Noop if the client is automatically following redirects.
+   * 
+   * @see isDidReceiveRedirect()
+   * @see doFollowRedirect()
+   * @return the page redirected to, or {@code null} if not in a position to redirect
+   */
   Page followRedirect() {
     if (redirectEnabled || didReceiveRedirect == false) {
       null
@@ -154,21 +199,51 @@ class WebSession {
     }
   }
   
+  /**
+   * If did receive a redirect, the url of the redirect request.
+   * 
+   * This is obtained by getting the 'Location' header of the response.
+   * 
+   * @see isDidReceiveRedirect()
+   * @return the url if did receive a redirect, otherwise {@code null}
+   */
   String getRedirectURL() {
     (didReceiveRedirect) ? response.getResponseHeaderValue('Location') : null
   }
-  
+
+  /**
+   * Make a new get request to the redirect URL.
+   * 
+   * Note: this should _only_ be called when isDidReceiveRedirect() returns true. This
+   * method does not check. Behaviour is unspecified if getRedirectURL() returns {@code null}.
+   * 
+   * @see getRedirectURL()
+   * @see get(Object, Closure)
+   */
+  protected doFollowRedirect() {
+    get(redirectURL)
+  }
+
+  /**
+   * Sets the client to a state suitable for use with this API.
+   * 
+   * The 'throwExceptionOnFailingStatusCode' is set to false.
+   */
   protected void configureWebClient(WebClient client) {
     client.with {
       throwExceptionOnFailingStatusCode = false
     }
   }
       
-  protected doFollowRedirect() {
-    get(redirectURL)
-  }
-  
-  protected makeRequestURL(targetURL, Page relativePage = null) {
+  /**
+   * Convert a psuedo URL into a URL object.
+   * 
+   * If targetURL is a valid URL, it is used as is. If it is an absolute path, then 
+   * a URL is constructed with it relative to base. Otherwise if relativePage is provided 
+   * then a URL relative to that is constructed, else a URL is constructed relative to the 
+   * current page if there is one or base is there is not.
+   */
+  protected URL makeRequestURL(targetURL, Page relativePage = null) {
     targetURL = targetURL as String
 
     try {
@@ -188,19 +263,29 @@ class WebSession {
     }
   }
 
-  protected makeRequestURLRelativeToBase(String urlPath) {
+  /**
+   * @return A URL of base + urlPath
+   */
+  protected URL makeRequestURLRelativeToBase(String urlPath) {
     if (this.base) {
       new URL(new URL(base), urlPath)
     } else {
       throw new IllegalArgumentException("Cannot makeRequestURLRelativeToBase for $urlPath as 'base' is not set".toString())
     }
   }
-  
-  protected makeRequestURLRelativeToPage(String urlPath, Page pageBase) {
+
+  /**
+   * @return A URL that is urlPath relative to pageBase
+   */  
+  protected URL makeRequestURLRelativeToPage(String urlPath, Page pageBase) {
     new URL(pageBase.webResponse.requestSettings.url, urlPath)
   }
-  
-  protected makeRequestURLRelativeToCurrentPageOrBase(String urlPath) {
+
+  /**
+   * @return A URL that is urlPath relative to the current page if there is one, or
+   *         relative to base if there is no current page.
+   */    
+  protected URL makeRequestURLRelativeToCurrentPageOrBase(String urlPath) {
     if (page) {
       new URL(requestURL, urlPath)
     } else {
@@ -208,7 +293,10 @@ class WebSession {
     }
   }
   
-  protected boolean isRedirectStatus(code) {
+  /**
+   * Convenience method for determining if the status is a redirect or not.
+   */
+  protected boolean isRedirectStatus(int code) {
     code in [300, 301, 302, 303, 307]
   }
   

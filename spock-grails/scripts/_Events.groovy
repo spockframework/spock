da0f015cfe4e0b9d2546340fa03ebf7cc686ab5d
Luke Daley <ld@ldaley.com>
2009-12-18 23:50:40 +0000

    Workaround a maven build issue by calling the grails build target that sets the properties of the functional test app deployment.

--- spock-grails/scripts/_Events.groovy
+++ spock-grails/scripts/_Events.groovy
@@ -23,6 +23,13 @@ eventAllTestsStart = {
 
 eventTestPhaseStart = { phaseName ->
   if (phaseName == 'functional') {
+    
+    // This is required when building with maven, to workaround a bug in grails-maven
+    if (!binding.hasProperty('serverContextPath')) {
+      includeTargets << grailsScript("_GrailsPackage") 
+      configureServerContextPath()
+    }
+    
     def functionalSpecificationClass = classLoader.loadClass("grails.plugin.spock.FunctionalSpecification")
     functionalSpecificationClass.baseUrl = argsMap["baseUrl"] ?: "http://localhost:$serverPort$serverContextPath"
   }

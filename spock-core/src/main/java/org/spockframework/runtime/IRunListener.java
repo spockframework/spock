98c74e515ef517097e0cd131daf44e192f6a4cec
Peter Niederwieser <pniederw@gmail.com>
2010-03-03 02:09:47 +0000

    polishing

--- spock-core/src/main/java/org/spockframework/runtime/IRunListener.java
+++ spock-core/src/main/java/org/spockframework/runtime/IRunListener.java
@@ -14,11 +14,13 @@
 
 package org.spockframework.runtime;
 
+import org.spockframework.runtime.extension.builtin.ScenarioExtension;
 import org.spockframework.runtime.model.*;
 
 /**
  * Listens to a spec run. Currently, only extensions can register listeners.
- * They do so by invoking <tt>SpecInfo.addListener()<tt>.
+ * They do so by invoking <tt>SpecInfo.addListener()<tt>. See
+ * {@link ScenarioExtension} for an example of how to use a listener.
  *
  * @author Peter Niederwieser
  */

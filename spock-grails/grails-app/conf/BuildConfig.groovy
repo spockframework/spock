c7af9f3b09b62bc6cf3a72d480cd89141de646b4
Peter Niederwieser <pniederw@gmail.com>
2010-04-27 23:50:59 +0000

    trying to fix spock-grails Maven build

--- spock-grails/grails-app/conf/BuildConfig.groovy
+++ spock-grails/grails-app/conf/BuildConfig.groovy
@@ -24,6 +24,7 @@ grails.project.dependency.resolution = {
 
   repositories {
     grailsHome()
+    grailsCentral()
 
     mavenLocal() // prefer local, so we pick up spock snapshot as part of whole build
     mavenRepo "http://m2repo.spockframework.org/snapshots"

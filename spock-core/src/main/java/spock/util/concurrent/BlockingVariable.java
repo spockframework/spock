c2afb9709a410d71ca6d6d62f741086eddf2f5b6
Peter Niederwieser <pniederw@gmail.com>
2010-05-06 22:50:19 +0000

    polishing

--- spock-core/src/main/java/spock/util/concurrent/BlockingVariable.java
+++ spock-core/src/main/java/spock/util/concurrent/BlockingVariable.java
@@ -37,7 +37,7 @@ public class BlockingVariable<T> {
 
   public T get() throws InterruptedException {
     if (!valueReady.await(timeout, unit))
-      throw new SpockTimeoutError("BlockingVariable.getValue() timed out after %d %s")
+      throw new SpockTimeoutError("BlockingVariable.get() timed out after %d %s")
           .withArgs(timeout, unit.toString().toLowerCase());
     return value;
   }

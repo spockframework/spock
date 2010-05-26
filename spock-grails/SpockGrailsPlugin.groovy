1b24b117b90ca8be59e5d3f9c795eae3f5ab92db
Peter Niederwieser <pniederw@gmail.com>
2010-05-26 07:27:18 +0000

    undid previous check-in

--- spock-grails/SpockGrailsPlugin.groovy
+++ spock-grails/SpockGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  */
 
 class SpockGrailsPlugin {
-  def version = "0.4-groovy-1.6"
+  def version = "0.4-groovy-1.6-SNAPSHOT"
   def grailsVersion = "1.2.0 > 1.2.99"
   def dependsOn = [:]
   def pluginExcludes = [

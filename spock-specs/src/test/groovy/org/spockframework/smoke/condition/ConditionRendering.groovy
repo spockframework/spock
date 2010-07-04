23f8fee659833a15253d98bbd97639c95a219c7b
Peter Niederwieser <pniederw@gmail.com>
2010-07-04 23:23:56 +0000

    fixed problem related to exotic method names and annotation closures

--- spock-specs/src/test/groovy/org/spockframework/smoke/condition/ConditionRendering.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/condition/ConditionRendering.groovy
@@ -682,6 +682,33 @@ three(a, b,c)
     }
   }
 
+  def "leading and trailing newlines are escaped"() {
+    expect:
+    isRendered """
+a == null
+| |
+| false
+"\\nfoo
+bar\\n"
+    """, {
+      def a = "\nfoo\nbar\n"
+      assert a == null
+    }
+  }
+
+  def "control characters other than newlines are escaped"() {
+    expect:
+    isRendered """
+a == null
+| |
+| false
+"\\b\\tfoo\\r\\f"
+    """, {
+      def a = "\b\tfoo\r\f"
+      assert a == null
+    }
+  }
+
   /*
   def "MapEntryExpression"() {
       // tested as part of testMapExpression
@@ -721,7 +748,7 @@ private class Person2 {
   def name
   def age
   def height
-  
+
   def eat(args) { null }
 
   String toString() { "p" }

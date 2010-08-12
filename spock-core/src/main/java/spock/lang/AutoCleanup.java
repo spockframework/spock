8b05819a6ec855633facf24e5706380941e685ee
Peter Niederwieser <pniederw@gmail.com>
2010-08-12 00:00:01 +0000

    polishing

--- spock-core/src/main/java/spock/lang/AutoCleanup.java
+++ spock-core/src/main/java/spock/lang/AutoCleanup.java
@@ -20,17 +20,18 @@ import org.spockframework.runtime.extension.ExtensionAnnotation;
 import org.spockframework.runtime.extension.builtin.AutoCleanupExtension;
 
 /**
- * Automatically cleans up the object stored in the annotated field or property.
- * Cleanup of an object has the same effect as, and serves as a convenient
- * replacement for, calling the object's <tt>close</tt> method at the end
- * of the spec's <tt>cleanup</tt> method. (<tt>&#64;Shared</tt> resources are
- * cleaned up at the end of the spec's <tt>cleanupSpec</tt> method.)
+ * Automatically cleans up the object stored in the annotated field or property
+ * at the end of its life time. More precisely, auto-cleanup of an object has
+ * the same effect as, and serves as a convenient replacement for, calling the
+ * object's <tt>close</tt> method at the end of the spec's <tt>cleanup</tt> method.
+ * <tt>&#64;Shared</tt> objects are cleaned up at the end of the spec's
+ * <tt>cleanupSpec</tt> method.
 
  * <h3>Customizing how cleanup is performed</h3>
  * By default, an object is cleaned up by invoking its parameterless close()
  * method (which is assumed to exist); visibility and return type of this method
  * are irrelevant. If some other method should be called instead, override the
- * annotation's <tt>value</tt> attribute as follows:
+ * annotation's <tt>value</tt> attribute:
  *
  * <pre>
  * &#64;AutoCleanup("dispose") // invoke the object's "dispose" method
@@ -44,8 +45,8 @@ import org.spockframework.runtime.extension.builtin.AutoCleanupExtension;
  * <h3>Handling of exceptions during cleanup</h3>
  * If a cleanup operation fails with an exception, the exception is reported
  * (just as if it had occurred in a <tt>cleanup</tt> or <tt>cleanupSpec</tt>
- * method) and cleanup proceeds with the next annotated object. To prevent a
- * cleanup exception from being reported, override the annotation's <tt>quiet</tt>
+ * method) and cleanup proceeds with the next annotated object. To prevent
+ * cleanup exceptions from being reported, override the annotation's <tt>quiet</tt>
  * attribute:
  *
  * <pre>

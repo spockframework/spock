9ce08e215140487833eca952d8f044ccbae22381
Peter Niederwieser <pniederw@gmail.com>
2010-12-07 00:48:48 +0000

    fixed issue 151: Shared private field in superclass cannot be accessed

---
+++ spock-specs/src/test/groovy/org/spockframework/smoke/SharedFieldsInSuperclass.groovy
@@ -0,0 +1,29 @@
+package org.spockframework.smoke
+
+import spock.lang.*
+
+abstract class SharedFieldsInSuperclassBase extends Specification {
+  @Shared private String sharedPrivate = "sharedPrivate"
+  @Shared protected String sharedProtected = "sharedProtected"
+  @Shared public String sharedPublic = "sharedPublic"
+  @Shared String sharedProperty = "sharedProperty"
+
+  @Issue("http://issues.spockframework.org/detail?id=151")
+  def "can access shared private field"() {
+    expect: sharedPrivate == "sharedPrivate"
+  }
+
+  def "can access shared protected field"() {
+    expect: sharedProtected == "sharedProtected"
+  }
+
+  def "can access shared public field"() {
+    expect: sharedPublic == "sharedPublic"
+  }
+
+  def "can access shared property"() {
+    expect: sharedProperty == "sharedProperty"
+  }
+}
+
+class SharedFieldsInSuperclass extends SharedFieldsInSuperclassBase {}

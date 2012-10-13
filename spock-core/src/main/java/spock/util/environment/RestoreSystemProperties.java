506d6e2f71819de38cf1fb7478c3edfd16400aad
Peter Niederwieser <pniederw@gmail.com>
2012-10-13 06:04:46 +0200

    added @RestoreSystemProperties extension

---
+++ spock-core/src/main/java/spock/util/environment/RestoreSystemProperties.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package spock.util.environment;
+
+import java.lang.annotation.*;
+
+import org.spockframework.runtime.extension.ExtensionAnnotation;
+import org.spockframework.runtime.extension.builtin.RestoreSystemPropertiesExtension;
+
+/**
+ * Saves system properties before the annotated feature method has been run, and restores them afterwards.
+ * The system properties are saved before any setup method is run, and are restored after all cleanup methods
+ * have run. Applying this annotation to a spec class has the same effect as applying it to all its feature methods.
+ */
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@ExtensionAnnotation(RestoreSystemPropertiesExtension.class)
+public @interface RestoreSystemProperties {}

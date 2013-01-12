03f1fe8f15b861ef5cdf88f8c2bd226036523896
Peter Niederwieser <pniederw@gmail.com>
2013-01-12 14:55:16 +0100

    fixed issue 291:	PollingConditions doesn't report failed assertion

--- spock-core/src/main/java/spock/util/concurrent/PollingConditions.java
+++ spock-core/src/main/java/spock/util/concurrent/PollingConditions.java
@@ -161,7 +161,7 @@ public class PollingConditions {
         long elapsedTime = lastAttempt - start;
         if (elapsedTime >= timeoutMillis) {
           String msg = String.format("Condition not satisfied after %1.2f seconds and %d attempts", elapsedTime / 1000d, attempts);
-          throw new SpockTimeoutError(seconds, msg);
+          throw new SpockTimeoutError(seconds, msg, e);
         }
         Thread.sleep(Math.min(currDelay, start + timeoutMillis - System.currentTimeMillis()));
         currDelay *= factor;

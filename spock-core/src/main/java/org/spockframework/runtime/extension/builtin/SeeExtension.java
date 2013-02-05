49040e5350c60b76dc2222e8e9c773709bce72c1
Peter Niederwieser <pniederw@gmail.com>
2013-02-05 05:36:07 +0100

    extension that turns @See into an attachment

---
+++ spock-core/src/main/java/org/spockframework/runtime/extension/builtin/SeeExtension.java
@@ -0,0 +1,27 @@
+package org.spockframework.runtime.extension.builtin;
+
+import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension;
+import org.spockframework.runtime.model.Attachment;
+import org.spockframework.runtime.model.FeatureInfo;
+import org.spockframework.runtime.model.SpecElementInfo;
+import org.spockframework.runtime.model.SpecInfo;
+
+import spock.lang.See;
+
+public class SeeExtension extends AbstractAnnotationDrivenExtension<See> {
+  @Override
+  public void visitSpecAnnotation(See see, SpecInfo spec) {
+    addAttachments(see, spec);
+  }
+
+  @Override
+  public void visitFeatureAnnotation(See see, FeatureInfo feature) {
+    addAttachments(see, feature);
+  }
+
+  private void addAttachments(See see, SpecElementInfo specElement) {
+    for (String url : see.value()) {
+      specElement.addAttachment(new Attachment(url, url));
+    }
+  }
+}

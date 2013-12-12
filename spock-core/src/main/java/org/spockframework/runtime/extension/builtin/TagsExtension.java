7622095574a03f2864542f037b2bf221978b531f
Grant Currey <grant.currey@flightcentre.com.au>
2013-12-12 10:21:38 +1000

    Added support for speicfication and feature tagging

---
+++ spock-core/src/main/java/org/spockframework/runtime/extension/builtin/TagsExtension.java
@@ -0,0 +1,38 @@
+package common.extension.spock.runtime
+
+import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension
+import org.spockframework.runtime.model.FeatureInfo
+import org.spockframework.runtime.model.ISkippable
+import org.spockframework.runtime.model.SpecInfo
+import common.extension.spock.lang.Tags
+
+/**
+ * User: gcurrey
+ * Date: 12/12/13
+ * Time: 9:33 AM
+ */
+public class TagsExtension extends AbstractAnnotationDrivenExtension<Tags> {
+    public void visitSpecAnnotation(Tags annotation, SpecInfo spec) {
+        doVisit(annotation, spec);
+    }
+
+    public void visitFeatureAnnotation(Tags annotation, FeatureInfo feature) {
+        doVisit(annotation, feature);
+    }
+
+    private void doVisit(Tags annotation, ISkippable skippable) {
+        String[] tags = annotation.value();
+        String[] suppliedTags = System.getProperty("tags") != null ? System.getProperty("tags").split(",") : null;
+
+        boolean execute = false;
+        if (!suppliedTags || suppliedTags.size() == 0){
+            execute = true;
+        } else {
+            for(String tag : tags){
+                execute = true;
+            }
+        }
+
+        skippable.setSkipped(!execute);
+    }
+}

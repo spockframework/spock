fc9b4b01c727ea89611606cffa9ca4915941ee3e
fuud <fuudtorrentsru@gmail.com>
2015-04-05 19:53:30 +0300

    Scheduler: foolproof

--- spock-core/src/main/java/org/spockframework/runtime/Scheduler.java
+++ spock-core/src/main/java/org/spockframework/runtime/Scheduler.java
@@ -8,8 +8,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Scheduler {
   private final RunnerScheduler runnerScheduler;
   private final boolean forceSequential;
-  private final AtomicInteger scheduledConcurrentTasks = new AtomicInteger(0);
+  private final AtomicInteger scheduledTasksCount = new AtomicInteger(0);
   private final Semaphore concurrentJobsSemaphore = new Semaphore(0);
+  private State state = State.SCHEDULING;
 
   public Scheduler(RunnerScheduler runnerScheduler, boolean forceSequential) {
     this.runnerScheduler = runnerScheduler;
@@ -17,10 +18,15 @@ public class Scheduler {
   }
 
   public void schedule(final Runnable task) {
+    synchronized (this){
+      if (state == State.WAITING) {
+        throw new IllegalStateException("No new tasks can be scheduled after waitFinished() was called");
+      }
+      scheduledTasksCount.incrementAndGet();
+    }
     if (forceSequential) {
       task.run();
     } else {
-      scheduledConcurrentTasks.incrementAndGet();
       runnerScheduler.schedule(new Runnable() {
         @Override
         public void run() {
@@ -35,9 +41,15 @@ public class Scheduler {
   }
 
   public void waitFinished() {
-    if (!forceSequential && scheduledConcurrentTasks.get() > 0) {
+    synchronized (this){
+      if (state == State.WAITING) {
+        throw new IllegalStateException("waitFinished() can be called only once");
+      }
+      state = State.WAITING;
+    }
+    if (!forceSequential && scheduledTasksCount.get() > 0) {
       try {
-        concurrentJobsSemaphore.acquire(scheduledConcurrentTasks.get());
+        concurrentJobsSemaphore.acquire(scheduledTasksCount.get());
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
@@ -46,5 +58,10 @@ public class Scheduler {
 
   public Scheduler deriveScheduler(boolean forceSequential) {
     return new Scheduler(runnerScheduler, forceSequential);
+  }
+
+  private enum State {
+    SCHEDULING,
+    WAITING
   }
 }

68cfbe2f640a7b0f1d338f7f299ea26fcdbd7c7c
Leonard Brünings <lord_damokles@gmx.net>
2017-07-20 00:59:59 +0200

    Properly detect Spring 2/3 ContextConfiguration (fixes #750)

--- spock-spring/spring2-test/src/test/groovy/org/spockframework/spring2/RuntimeCompatibilitySpec.groovy
+++ spock-spring/spring2-test/src/test/groovy/org/spockframework/spring2/RuntimeCompatibilitySpec.groovy
@@ -1,14 +1,26 @@
 package org.spockframework.spring2
 
-import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+
+import java.util.concurrent.Executor
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
 
 @ContextConfiguration
 class RuntimeCompatibilitySpec extends Specification {
 
+  @Autowired
+  Executor injectMe
+
   def "no runtime errors are thrown"() {
     expect:
     1 == 1
+  }
+
+  def "injection works"() {
+    expect:
+    injectMe != null
   }
 
 }

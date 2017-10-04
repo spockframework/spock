spockframework/spock

commit f1d7a29387c974cafaf02f8f62f41a22a687f6c5

Enable early injection for Spring ...
by moving `prepareTestInstance` from `interceptSetupMethod` to
`interceptInitializerMethod` which is called directly after instance
creation and is already used by the `GuiceExtension`
and `TapestryExtension`.

spock-spring/src/main/java/org/spockframework/spring/SpringInterceptor.java
@@ -38,8 +38,12 @@ public class SpringInterceptor extends AbstractMethodInterceptor {
   }
 
   @Override
-  public void interceptSetupMethod(IMethodInvocation invocation) throws Throwable {
+  public void interceptInitializerMethod(IMethodInvocation invocation) throws Throwable {
     manager.prepareTestInstance(invocation.getInstance());
+  }
+
+  @Override
+  public void interceptSetupMethod(IMethodInvocation invocation) throws Throwable {
     exception = null;
     beforeTestMethodInvoked = true;
     manager.beforeTestMethod(invocation.getInstance(),

spock-spring/src/main/java/org/spockframework/spring/SpringExtension.java
@@ -72,6 +72,7 @@ public class SpringExtension extends AbstractGlobalExtension {
     });
 
     spec.addSetupSpecInterceptor(interceptor);
+    spec.addInitializerInterceptor(interceptor);
     spec.addSetupInterceptor(interceptor);
     spec.addCleanupInterceptor(interceptor);
     spec.addCleanupSpecInterceptor(interceptor);

commit 61b809fcf3f3572c918623d2e59736b1b5d593ab

Fix bug in SpringExtension that disabled initializers
The bug was introduced by f1d7a29387c974cafaf02f8f62f41a22a687f6c5

spock-spring/src/main/java/org/spockframework/spring/SpringInterceptor.java
@@ -39,6 +39,7 @@ public class SpringInterceptor extends AbstractMethodInterceptor {
 
   @Override
   public void interceptInitializerMethod(IMethodInvocation invocation) throws Throwable {
+    invocation.proceed(); // needs to run before so that mocks are already initialized
     manager.prepareTestInstance(invocation.getInstance());
   }
 

spock-spring/src/test/groovy/org/spockframework/spring/MockInjectionWithEmbeddedConfig.groovy
@@ -1,14 +1,13 @@
 package org.spockframework.spring
 
-import spock.mock.DetachedMockFactory
 import org.spockframework.mock.MockUtil
+import spock.lang.Specification
+import spock.mock.DetachedMockFactory
+
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.*
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.support.AnnotationConfigContextLoader
-
-import spock.lang.Specification
 
 @ContextConfiguration(loader=AnnotationConfigContextLoader)
 class MockInjectionWithEmbeddedConfig extends Specification {
@@ -34,6 +33,8 @@ class MockInjectionWithEmbeddedConfig extends Specification {
   @Autowired
   IService2 service2
 
+  List normalMock = Mock()
+
   def "Injected services are mocks"() {
     expect:
     new MockUtil().isMock(service1)
@@ -54,6 +55,14 @@ class MockInjectionWithEmbeddedConfig extends Specification {
     expect:
     service1.generateString() == null
     service2.generateQuickBrownFox() == null
+  }
 
+  def "normal mocks work too"() {
+    when:
+    def result = normalMock.get(0)
+
+    then:
+    result == "hello"
+    1 * normalMock.get(_) >> 'hello'
   }
 }
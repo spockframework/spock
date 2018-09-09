a463f1bea31959c4416ad66607705386bc653710
Marc Philipp <marc@gradle.com>
2018-09-09 16:53:45 +0200

    Use Lookup class loading strategy if available (#895)
    In order to create mock objects on Java 11+, `ProxyBasedMockFactory`
    now uses ByteBuddy's `Lookup`-based `ClassLoadingStrategy`, if
    `Reflection` strategy is not available.
    
    Resolves #843

--- spock-core/src/main/java/org/spockframework/mock/runtime/ProxyBasedMockFactory.java
+++ spock-core/src/main/java/org/spockframework/mock/runtime/ProxyBasedMockFactory.java
@@ -14,7 +14,11 @@
 
 package org.spockframework.mock.runtime;
 
+import net.bytebuddy.dynamic.loading.ClassInjector;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import org.jetbrains.annotations.NotNull;
 import org.spockframework.mock.*;
+import org.spockframework.mock.codegen.Target;
 import org.spockframework.runtime.InvalidSpecException;
 import org.spockframework.util.*;
 
@@ -22,6 +26,7 @@ import java.lang.reflect.*;
 import java.lang.reflect.Proxy;
 import java.util.*;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
 
 import net.bytebuddy.*;
 import net.bytebuddy.description.modifier.*;
@@ -89,6 +94,8 @@ public class ProxyBasedMockFactory {
 
     private static final TypeCache<TypeCache.SimpleKey> CACHE =
       new TypeCache.WithInlineExpunction<>(TypeCache.Sort.SOFT);
+    private static final Class<?> CODEGEN_TARGET_CLASS = Target.class;
+    private static final String CODEGEN_PACKAGE = CODEGEN_TARGET_CLASS.getPackage().getName();
 
     static Object createMock(final Class<?> type,
                              final List<Class<?>> additionalInterfaces,
@@ -102,11 +109,20 @@ public class ProxyBasedMockFactory {
         new Callable<Class<?>>() {
           @Override
           public Class<?> call() throws Exception {
+            String typeName = type.getName();
+            Class<?> targetClass = type;
+            if (shouldLoadIntoCodegenPackage(type)) {
+              typeName = CODEGEN_PACKAGE + "." + type.getSimpleName();
+              targetClass = CODEGEN_TARGET_CLASS;
+            }
+            int randomNumber = Math.abs(ThreadLocalRandom.current().nextInt());
+            String name = String.format("%s$%s$%d", typeName, "SpockMock", randomNumber);
+            ClassLoadingStrategy<ClassLoader> strategy = determineBestClassLoadingStrategy(targetClass);
             return new ByteBuddy()
-              .with(new NamingStrategy.SuffixingRandom("SpockMock"))
               .with(TypeValidation.DISABLED) // https://github.com/spockframework/spock/issues/776
               .ignore(none())
               .subclass(type)
+              .name(name)
               .implement(additionalInterfaces)
               .implement(ISpockMockObject.class)
               .method(any())
@@ -118,7 +134,7 @@ public class ProxyBasedMockFactory {
               .intercept(FieldAccessor.ofField("$spock_interceptor"))
               .defineField("$spock_interceptor", IProxyBasedMockInterceptor.class, Visibility.PRIVATE)
               .make()
-              .load(classLoader)
+              .load(classLoader, strategy)
               .getLoaded();
           }
         }, CACHE);
@@ -126,6 +142,43 @@ public class ProxyBasedMockFactory {
       Object proxy = MockInstantiator.instantiate(type, enhancedType, constructorArgs, useObjenesis);
       ((ByteBuddyInterceptorAdapter.InterceptorAccess) proxy).$spock_set(interceptor);
       return proxy;
+    }
+
+    // This methods and the ones it calls are inspired by similar code in Mockito's SubclassBytecodeGenerator
+    private static boolean shouldLoadIntoCodegenPackage(Class<?> type) {
+      return isComingFromJDK(type) || isComingFromSignedJar(type) || isComingFromSealedPackage(type);
+    }
+
+    private static boolean isComingFromJDK(Class<?> type) {
+      // Try to read Implementation-Title entry from manifest which isn't present in every JDK JAR
+      return type.getPackage() != null && "Java Runtime Environment".equalsIgnoreCase(type.getPackage().getImplementationTitle())
+        || type.getName().startsWith("java.")
+        || type.getName().startsWith("javax.");
+    }
+
+    private static boolean isComingFromSealedPackage(Class<?> type) {
+      return type.getPackage() != null && type.getPackage().isSealed();
+    }
+
+    private static boolean isComingFromSignedJar(Class<?> type) {
+      return type.getSigners() != null;
+    }
+
+    @NotNull
+    private static ClassLoadingStrategy<ClassLoader> determineBestClassLoadingStrategy(Class<?> targetClass) throws Exception {
+      if (ClassInjector.UsingLookup.isAvailable()) {
+        Class<?> methodHandlesClass = Class.forName("java.lang.invoke.MethodHandles");
+        Class<?> lookupClass = Class.forName("java.lang.invoke.MethodHandles$Lookup");
+        Method lookupMethod = methodHandlesClass.getMethod("lookup");
+        Method privateLookupInMethod = methodHandlesClass.getMethod("privateLookupIn", Class.class, lookupClass);
+        Object lookup = lookupMethod.invoke(null);
+        Object privateLookup = privateLookupInMethod.invoke(null, targetClass, lookup);
+        return ClassLoadingStrategy.UsingLookup.of(privateLookup);
+      }
+      if (ClassInjector.UsingReflection.isAvailable()) {
+        return ClassLoadingStrategy.Default.INJECTION;
+      }
+      return ClassLoadingStrategy.Default.WRAPPER;
     }
   }
 

9296e9c9642da406aeafa22742b6f8eab03c4e90
Marc Philipp <mail@marcphilipp.de>
2018-09-11 15:24:43 +0200

    Separate code specific to cglib/ByteBuddy
    This refactoring commit extracts three top-level classes from
    `ProxyBasedMockFactory`: one for generating dynamic proxies for
    interfaces, one for cglib-specific code, and one for ByteBuddy-specific
    code.

---
+++ spock-core/src/main/java/org/spockframework/mock/runtime/DynamicProxyMockFactory.java
@@ -0,0 +1,30 @@
+package org.spockframework.mock.runtime;
+
+import org.spockframework.mock.ISpockMockObject;
+import org.spockframework.mock.runtime.DynamicProxyMockInterceptorAdapter;
+import org.spockframework.mock.runtime.IProxyBasedMockInterceptor;
+import org.spockframework.runtime.InvalidSpecException;
+
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
+class DynamicProxyMockFactory {
+
+  static Object createMock(Class<?> mockType, List<Class<?>> additionalInterfaces,
+                                  List<Object> constructorArgs, IProxyBasedMockInterceptor mockInterceptor, ClassLoader classLoader) {
+    if (constructorArgs != null) {
+      throw new InvalidSpecException("Interface based mocks may not have constructor arguments");
+    }
+    List<Class<?>> interfaces = new ArrayList<>();
+    interfaces.add(mockType);
+    interfaces.addAll(additionalInterfaces);
+    interfaces.add(ISpockMockObject.class);
+    return Proxy.newProxyInstance(
+        classLoader,
+        interfaces.toArray(new Class<?>[interfaces.size()]),
+        new DynamicProxyMockInterceptorAdapter(mockInterceptor)
+    );
+  }
+
+}

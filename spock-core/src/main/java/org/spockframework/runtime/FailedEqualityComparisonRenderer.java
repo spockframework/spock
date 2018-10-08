fd5d99bfc5c9f44c9a58948c4e74b415c7df8057
Björn Kautler <Bjoern@Kautler.net>
2018-10-08 10:11:12 +0200

    Render irrelevant expressions if they get a type hint in comparison failure

--- spock-core/src/main/java/org/spockframework/runtime/FailedEqualityComparisonRenderer.java
+++ spock-core/src/main/java/org/spockframework/runtime/FailedEqualityComparisonRenderer.java
@@ -3,6 +3,10 @@ package org.spockframework.runtime;
 import org.spockframework.runtime.model.ExpressionInfo;
 import org.spockframework.util.ObjectUtil;
 
+import java.util.Objects;
+
+import static org.spockframework.runtime.ExpressionInfoValueRenderer.renderValue;
+
 public class FailedEqualityComparisonRenderer implements ExpressionComparisonRenderer {
   @Override
   public String render(ExpressionInfo expr) {
@@ -11,9 +15,18 @@ public class FailedEqualityComparisonRenderer implements ExpressionComparisonRen
 
     ExpressionInfo expr1 = expr.getChildren().get(0);
     ExpressionInfo expr2 = expr.getChildren().get(1);
-    if (expr1.getEffectiveRenderedValue().equals(expr2.getEffectiveRenderedValue())) {
+    if (expr1.getRenderedValue() == null) renderIrrelevant(expr1);
+    if (expr2.getRenderedValue() == null) renderIrrelevant(expr2);
+    if (Objects.equals(expr1.getRenderedValue(), expr2.getRenderedValue())) {
       addTypeHint(expr1);
+      expr1.setRelevant(true);
       addTypeHint(expr2);
+      expr2.setRelevant(true);
+    }
+
+    if (Objects.equals(expr1.getRenderedValue(), expr2.getRenderedValue())) {
+      addIdentityHashCode(expr1);
+      addIdentityHashCode(expr2);
     }
 
     return "false";
@@ -24,5 +37,19 @@ public class FailedEqualityComparisonRenderer implements ExpressionComparisonRen
 
     Class<?> exprType = ObjectUtil.voidAwareGetClass(expr.getValue());
     expr.setRenderedValue(expr.getRenderedValue() + " (" + exprType.getName() + ")");
+  }
+
+  private void renderIrrelevant(ExpressionInfo irrelevantExpr) {
+    for (ExpressionInfo expr : irrelevantExpr.inPostfixOrder(false)) {
+      if (!expr.isRelevant() && (expr.getRenderedValue() == null)) {
+        expr.setRenderedValue(renderValue(expr));
+      }
+    }
+  }
+
+  private void addIdentityHashCode(ExpressionInfo expr) {
+    if (expr.getRenderedValue() == null) return;
+
+    expr.setRenderedValue(String.format("%s @%08x", expr.getRenderedValue(), System.identityHashCode(expr.getValue())));
   }
 }

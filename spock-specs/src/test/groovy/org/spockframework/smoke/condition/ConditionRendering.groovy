93598c0603b907d22f2a6cdcca229794a9373a7d
Björn Kautler <Bjoern@Kautler.net>
2018-10-14 16:03:32 +0200

    Fix erroneous regex where an optional colon was defined instead of a non-capturing group (#931)
    * Fix erroneous regex where an optional colon was defined instead of a non-capturing group

--- spock-specs/src/test/groovy/org/spockframework/smoke/condition/ConditionRendering.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/condition/ConditionRendering.groovy
@@ -724,7 +724,18 @@ ${func.toString()}
     """, {
       assert func.call(42) == null
     }
-}
+  }
+
+  def "properly escape backslashes in rendering"() {
+    expect:
+    isRendered """
+":\\":\\\\" == null
+         |
+         false
+    """, {
+      assert ":\":\\" == null
+    }
+  }
 
   /*
   def "MapEntryExpression"() {

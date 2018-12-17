55649bb6ec9c658aa568f035488c0afc0e04d535
Sapan Parikh <sapanparikh18@gmail.com>
2018-12-17 11:41:30 +0530

    An annotation, FeatureInfo, to add information about some of the basic test attributes. This annotation then can be integrated with Spock Reports to add the following 1. Id (Need to identify every feature uniquely) 2. Impact (Impact of the feature, if it fails) 3. Frequency of Failure (Probability of a feature failing) 4. Component (Which component it may be associated with. Example: shopping cart, product page etc )
    These params then cab be used to create reports like ACC (https://testing.googleblog.com/2011/10/google-test-analytics-now-in-open.html)

---
+++ spock-core/src/main/java/spock/lang/FeatureInfo.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package spock.lang;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+/**
+ * Allows to attaching id, impact, component and frequency of failure along with any addional data.
+ * This can be used in ACC analysis and risk based testing
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface FeatureInfo {
+  String id() default "";
+  int impact() default 1;
+  int frequencyOfFailure() default 1;
+  String component() default "";
+  String[] additionalInfo() default {};
+}

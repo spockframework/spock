f576e6e21331404f5bcc35280d867adf37206006
Marc Philipp <mail@marcphilipp.de>
2019-01-20 20:44:49 +0100

    Use new JUnit Platform discovery mechanism

---
+++ spock-core/src/main/java/org/spockframework/runtime/MethodSelectorResolver.java
@@ -0,0 +1,116 @@
+package org.spockframework.runtime;
+
+import org.junit.platform.engine.DiscoverySelector;
+import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.UniqueId;
+import org.junit.platform.engine.discovery.MethodSelector;
+import org.junit.platform.engine.support.discovery.SelectorResolver;
+import org.spockframework.runtime.model.FeatureInfo;
+import org.spockframework.runtime.model.IterationInfo;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectUniqueId;
+
+public class MethodSelectorResolver implements SelectorResolver {
+
+  private static final Object[] EMPTY_ARGS = new Object[0];
+
+  @Override
+  public Set<Class<? extends DiscoverySelector>> getSupportedSelectorTypes() {
+    return singleton(MethodSelector.class);
+  }
+
+  @Override
+  public Optional<Result> resolveSelector(DiscoverySelector selector, Context context) {
+    if (selector instanceof MethodSelector) {
+      MethodSelector methodSelector = (MethodSelector) selector;
+      return context
+        .addToParent(() -> selectClass(methodSelector.getJavaClass()), parent -> {
+          if (parent instanceof SpecNode) {
+            String methodName = methodSelector.getMethodName();
+            Optional<SpockNode> node = toNode((SpecNode) parent, feature -> feature.getFeatureMethod().getReflection().getName().equals(methodName));
+            if (!node.isPresent()) {
+              node = toNode((SpecNode) parent, feature -> methodName.equals(feature.getName()));
+            }
+            return node;
+          }
+          return Optional.empty();
+        })
+        .map(this::toResult);
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<Result> resolveUniqueId(UniqueId uniqueId, Context context) {
+    UniqueId.Segment lastSegment = uniqueId.getLastSegment();
+    if ("feature".equals(lastSegment.getType())) {
+      return context
+        .addToParent(() -> selectUniqueId(uniqueId.removeLastSegment()), parent -> {
+          if (parent instanceof SpecNode) {
+            String methodName = lastSegment.getValue();
+            return toNode((SpecNode) parent, feature -> methodName.equals(feature.getFeatureMethod().getReflection().getName()));
+          }
+          return Optional.empty();
+        })
+        .map(this::toResult);
+    }
+    if ("iteration".equals(lastSegment.getType())) {
+      return context
+        .addToParent(() -> selectUniqueId(uniqueId.removeLastSegment()), parent -> {
+          if (parent instanceof FeatureNode) {
+            FeatureNode featureNode = (FeatureNode) parent;
+            int iterationIndex = Integer.parseInt(lastSegment.getValue());
+            // TODO Add iterationIndex as allowed index to featureNode
+          }
+          return Optional.empty();
+        })
+        .map(node -> toResult(node).withPerfectMatch(false));
+    }
+    return Optional.empty();
+  }
+
+  private Optional<SpockNode> toNode(SpecNode specNode, Predicate<FeatureInfo> predicate) {
+    return specNode.getSpecInfo().getAllFeatures().stream()
+      .filter(predicate)
+      .filter(feature -> !feature.isExcluded())
+      .findAny()
+      .map(feature -> createNode(specNode, feature));
+  }
+
+  private SpockNode createNode(SpecNode specNode, FeatureInfo feature) {
+    if (feature.isParameterized()) {
+      return describeFeature(specNode.getUniqueId(), feature);
+    } else {
+      return describeSimpleIteration(specNode.getUniqueId(), feature);
+    }
+  }
+
+  private FeatureNode describeFeature(UniqueId parentId, FeatureInfo feature) {
+    return new FeatureNode(toUniqueId(parentId, feature), feature);
+  }
+
+  private IterationNode describeSimpleIteration(UniqueId parentId, FeatureInfo feature) {
+    IterationInfo iterationInfo = new IterationInfo(feature, EMPTY_ARGS, 1);
+    iterationInfo.setName(feature.getName());
+    return new IterationNode(toUniqueId(parentId, feature), iterationInfo);
+  }
+
+  private UniqueId toUniqueId(UniqueId parentId, FeatureInfo feature) {
+    return parentId.append("feature", feature.getFeatureMethod().getReflection().getName());
+  }
+
+  private Result toResult(TestDescriptor node) {
+    return Result.of(Match.of(node, () -> {
+      // TODO allow all iteration indexes
+      return emptySet();
+    }));
+  }
+
+}

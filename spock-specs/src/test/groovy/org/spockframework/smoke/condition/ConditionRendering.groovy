73ec707ed186e769ad65ae376c4acf266cdde3d4
Marcin Zajączkowski <szpak@users.sourceforge.net>
2020-01-13 10:10:27 +0100

    [Groovy3] Adjust error rendering tests to Groovy 2/3
    Power assertions in Groovy 3 renders resolved values at
    the beginning of the expression, not in the middle of it.

--- spock-specs/src/test/groovy/org/spockframework/smoke/condition/ConditionRendering.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/condition/ConditionRendering.groovy
@@ -16,9 +16,11 @@
 
 package org.spockframework.smoke.condition
 
-import org.spockframework.runtime.ConditionNotSatisfiedError
 import org.spockframework.runtime.SpockComparisonFailure
+import org.spockframework.util.GroovyVersionUtil
 import spock.lang.Issue
+import spock.lang.Requires
+
 import java.sql.Date
 
 import static java.lang.Math.min
@@ -356,12 +358,26 @@ a[b]
     }
   }
 
-  def "PostfixExpression"() {
+  @Requires({ GroovyVersionUtil.isGroovy2() })
+  def "PostfixExpression (Groovy 2)"() {
     expect:
     isRendered """
 x++ == null
  |  |
  0  false
+    """, {
+      def x = 0
+      assert x++ == null
+    }
+  }
+
+  @Requires({ !GroovyVersionUtil.isGroovy2() })
+  def "PostfixExpression"() {
+    expect:
+    isRendered """
+x++ == null
+|   |
+0   false
     """, {
       def x = 0
       assert x++ == null
@@ -541,10 +557,24 @@ interface java.util.List
     }
   }
 
-  def "ClassExpression with dot-containing comments"() {
+  @Requires({ GroovyVersionUtil.isGroovy2() })  //comments are no longer included in power assertion's error message in Groovy 3
+  def "ClassExpression with dot-containing comments (Groovy 2)"() {
     expect:
     isRendered """
 java.util./*.awt.*/List == java.lang.String // I. Like. Dots.
+                   |    |            |
+                   |    false        class java.lang.String
+                   interface java.util.List
+    """, {
+      assert java.util./*.awt.*/List == java.lang.String // I. Like. Dots.
+    }
+  }
+
+  @Requires({ !GroovyVersionUtil.isGroovy2() })
+  def "ClassExpression with dot-containing comments"() {
+    expect:
+    isRendered """
+java.util./*.awt.*/List == java.lang.String
                    |    |            |
                    |    false        class java.lang.String
                    interface java.util.List

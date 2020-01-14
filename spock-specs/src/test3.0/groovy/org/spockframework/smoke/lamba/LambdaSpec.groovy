399de9eb4364dd6bf4e7f44fd1d04a886e55fd3e
Marcin Zajączkowski <szpak@users.sourceforge.net>
2020-01-14 12:03:47 +0100

    [Groovy3] Basic tests with lambdas and method references

---
+++ spock-specs/src/test3.0/groovy/org/spockframework/smoke/lamba/LambdaSpec.groovy
@@ -0,0 +1,26 @@
+package org.spockframework.smoke.lamba
+
+import spock.lang.Specification
+
+import static java.util.stream.Collectors.toList
+
+class LambdaSpec extends Specification {
+
+  private List<String> names = ["Fred", "Wilma", "Barney", "Betty"]
+
+  def "allow to use Java lambda in spec"() {
+    when:
+    List<String> longNames = names.stream().filter(name -> name.length() > 5).collect(toList())
+
+    then:
+    longNames == ["Barney"]
+  }
+
+  def "allow to use method reference in spec"() {
+    when:
+    List<String> namesFromShortest = names.stream().sorted(Comparator.comparingInt(String::length).reversed()).collect(toList())
+
+    then:
+    namesFromShortest == ["Barney", "Wilma", "Betty", "Fred"]
+  }
+}

b8f03f62bb4d7e6f34494da1093ae577471288bf
Marcin Zajączkowski <szpak@users.sourceforge.net>
2020-03-23 22:09:57 +0100

    Remove JUnit 4 also from spock-core compile dependencies

---
+++ spock-core/src/main/java/org/spockframework/util/JUnit4LegacyUtil.java
@@ -0,0 +1,33 @@
+package org.spockframework.util;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * The utility class to deal with the JUnit 4 annotations in Spock in the way which allows to use Spock 2
+ * without junit4.jar at runtime.
+ *
+ * @since 3.0.0
+ */
+@SuppressWarnings("unchecked")
+public class JUnit4LegacyUtil {
+  private static final boolean IS_JUNIT4_AVAILABLE =
+    ReflectionUtil.loadClassIfAvailable("org.spockframework.junit4.ExceptionAdapterExtension") != null;
+
+  public static final Class<? extends Annotation> BEFORE_ANNOTATION =
+    (Class<? extends Annotation>) ReflectionUtil.loadClassIfAvailable("org.junit.Before");
+  public static final Class<? extends Annotation> AFTER_ANNOTATION =
+    (Class<? extends Annotation>) ReflectionUtil.loadClassIfAvailable("org.junit.After");
+  public static final Class<? extends Annotation> BEFORE_CLASS_ANNOTATION =
+    (Class<? extends Annotation>) ReflectionUtil.loadClassIfAvailable("org.junit.BeforeClass");
+  public static final Class<? extends Annotation> AFTER_CLASS_ANNOTATION =
+    (Class<? extends Annotation>) ReflectionUtil.loadClassIfAvailable("org.junit.AfterClass");
+
+  public static final Class<? extends Annotation> TEST_ANNOTATION =
+    (Class<? extends Annotation>) ReflectionUtil.loadClassIfAvailable("org.junit.Test");
+  public static final Class<? extends Annotation> RUN_WITH_ANNOTATION =
+    (Class<? extends Annotation>) ReflectionUtil.loadClassIfAvailable("org.junit.runner.RunWith");
+
+  public static boolean isJunit4SupportAvailable() {
+    return IS_JUNIT4_AVAILABLE;
+  }
+}

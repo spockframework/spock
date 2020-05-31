da44202e48b2647ff0dbf203407fd4bcf9359f84
Thomas Hirsch <thokari@posteo.de>
2020-05-31 17:54:25 +0200

    add AsyncConditionsDocSpec

---
+++ spock-specs/src/test/groovy/org/spockframework/docs/utilities/concurrent/AsyncConditionsDocSpec.groovy
@@ -0,0 +1,50 @@
+package org.spockframework.docs.utilities.concurrent
+
+import spock.lang.Specification
+import spock.util.concurrent.AsyncConditions
+
+import java.util.concurrent.TimeUnit
+
+class AsyncConditionsDocSpec extends Specification {
+
+// tag::async-conditions-spec[]
+  def "example for single passing evaluation"() {
+    def conditions = new AsyncConditions() // <1>
+
+    when:
+    Thread.start { // <2>
+      conditions.evaluate { //<3>
+        assert true
+      }
+    }
+
+    then:
+    conditions.await() // <4>
+  }
+
+  def "example for multiple passing evaluations"() {
+    def conditions = new AsyncConditions(3) // <5>
+
+    when:
+    Thread.start {
+      conditions.evaluate { // <6>
+        assert true
+        assert true
+      }
+      conditions.evaluate { // <6>
+        assert true
+      }
+    }
+
+    and:
+    Thread.start {
+      conditions.evaluate { // <6>
+        assert true
+      }
+    }
+
+    then:
+    conditions.await(2.5) // <7>
+  }
+}
+// end::async-conditions-spec[]

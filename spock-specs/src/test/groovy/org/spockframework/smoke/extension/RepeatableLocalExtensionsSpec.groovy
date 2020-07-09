74a4a9727e6d69e29e297f688561f9eb757401b0
Björn Kautler <Bjoern@Kautler.net>
2020-07-09 14:07:59 +0200

    incorporate review feedback vol. 3

--- spock-specs/src/test/groovy/org/spockframework/smoke/extension/RepeatableLocalExtensionsSpec.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/extension/RepeatableLocalExtensionsSpec.groovy
@@ -28,32 +28,6 @@ import java.lang.annotation.Repeatable
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 
-/**
- * The tests in this class use a repeatable annotation {@code @Foo} with a
- * container annotation {@code @FooContainer} and an extension {@code @FooExtension}
- * that sets static fields in the ran specification to count how often the extension
- * was triggered by the annotations.
- *
- * <p>The tests are all data-driven features that test a different scenario
- * (applied to spec, feature, fixture, field) with different combinations of
- * individual standalone {@code @Foo} annotations and ones contained within the
- * container annotation.
- *
- * <p>The data variables meanings are:
- * <dl>
- *   <dt>individual</dt>
- *   <dd>How many individual standalone annotations to generate</dd>
- *
- *   <dt>contained</dt>
- *   <dd>How many contained annotations to generate (-1 means omit the container completely)</dd>
- *
- *   <dt>containerWithList</dt>
- *   <dd>Whether to use list syntax for the container value or not</dd>
- *
- *   <dt>expected</dt>
- *   <dd>How many extension invocations are expected</dd>
- * </dl>
- */
 @ConfineMetaClassChanges(EmbeddedSpecRunner)
 class RepeatableLocalExtensionsSpec extends EmbeddedSpecification {
   def setup() {
@@ -77,8 +51,7 @@ class FooSpec extends Specification {
   def "repeated annotations work properly on spec"() {
     expect:
     runner.runWithImports """
-${buildContainer(contained, containerWithList)}
-${'@Foo\n' * individual}
+$annotations
 class FooSpec extends Specification {
   static specAnnotationsVisited = 0
   static featureAnnotationsVisited = 0
@@ -98,27 +71,26 @@ class FooSpec extends Specification {
 """
 
     where:
-    individual | contained | containerWithList || expected
-    0          | -1        | false             || 0
-    1          | -1        | false             || 1
-    2          | -1        | false             || 2
-    0          | 0         | false             || 0
-    0          | 0         | true              || 0
-    1          | 0         | false             || 1
-    1          | 0         | true              || 1
-    0          | 1         | false             || 1
-    0          | 1         | true              || 1
-    1          | 1         | false             || 2
-    1          | 1         | true              || 2
-    0          | 2         | true              || 2
-    1          | 2         | true              || 3
+    annotations                             || expected
+    ''                                      || 0
+    '@Foo'                                  || 1
+    '@Foo @Foo'                             || 2
+    '          @FooContainer()'             || 0
+    '          @FooContainer([])'           || 0
+    '@Foo      @FooContainer()'             || 1
+    '@Foo      @FooContainer([])'           || 1
+    '          @FooContainer(@Foo)'         || 1
+    '          @FooContainer([@Foo])'       || 1
+    '@Foo      @FooContainer(@Foo)'         || 2
+    '@Foo      @FooContainer([@Foo])'       || 2
+    '          @FooContainer([@Foo, @Foo])' || 2
+    '@Foo      @FooContainer([@Foo, @Foo])' || 3
   }
 
   def "repeated annotations work properly on feature"() {
     expect:
     runner.runSpec """
-${buildContainer(contained, containerWithList)}
-${'@Foo\n' * individual}
+$annotations
 def foo() {
   expect:
   verifyAll {
@@ -131,27 +103,26 @@ def foo() {
 """
 
     where:
-    individual | contained | containerWithList || expected
-    0          | -1        | false             || 0
-    1          | -1        | false             || 1
-    2          | -1        | false             || 2
-    0          | 0         | false             || 0
-    0          | 0         | true              || 0
-    1          | 0         | false             || 1
-    1          | 0         | true              || 1
-    0          | 1         | false             || 1
-    0          | 1         | true              || 1
-    1          | 1         | false             || 2
-    1          | 1         | true              || 2
-    0          | 2         | true              || 2
-    1          | 2         | true              || 3
+    annotations                             || expected
+    ''                                      || 0
+    '@Foo'                                  || 1
+    '@Foo @Foo'                             || 2
+    '          @FooContainer()'             || 0
+    '          @FooContainer([])'           || 0
+    '@Foo      @FooContainer()'             || 1
+    '@Foo      @FooContainer([])'           || 1
+    '          @FooContainer(@Foo)'         || 1
+    '          @FooContainer([@Foo])'       || 1
+    '@Foo      @FooContainer(@Foo)'         || 2
+    '@Foo      @FooContainer([@Foo])'       || 2
+    '          @FooContainer([@Foo, @Foo])' || 2
+    '@Foo      @FooContainer([@Foo, @Foo])' || 3
   }
 
   def "repeated annotations work properly on fixture"() {
     expect:
     runner.runSpec """
-${buildContainer(contained, containerWithList)}
-${'@Foo\n' * individual}
+$annotations
 def setup() {
 }
 
@@ -167,27 +138,26 @@ def foo() {
 """
 
     where:
-    individual | contained | containerWithList || expected
-    0          | -1        | false             || 0
-    1          | -1        | false             || 1
-    2          | -1        | false             || 2
-    0          | 0         | false             || 0
-    0          | 0         | true              || 0
-    1          | 0         | false             || 1
-    1          | 0         | true              || 1
-    0          | 1         | false             || 1
-    0          | 1         | true              || 1
-    1          | 1         | false             || 2
-    1          | 1         | true              || 2
-    0          | 2         | true              || 2
-    1          | 2         | true              || 3
+    annotations                             || expected
+    ''                                      || 0
+    '@Foo'                                  || 1
+    '@Foo @Foo'                             || 2
+    '          @FooContainer()'             || 0
+    '          @FooContainer([])'           || 0
+    '@Foo      @FooContainer()'             || 1
+    '@Foo      @FooContainer([])'           || 1
+    '          @FooContainer(@Foo)'         || 1
+    '          @FooContainer([@Foo])'       || 1
+    '@Foo      @FooContainer(@Foo)'         || 2
+    '@Foo      @FooContainer([@Foo])'       || 2
+    '          @FooContainer([@Foo, @Foo])' || 2
+    '@Foo      @FooContainer([@Foo, @Foo])' || 3
   }
 
   def "repeated annotations work properly on field"() {
     expect:
     runner.runSpec """
-${buildContainer(contained, containerWithList)}
-${'@Foo\n' * individual}
+$annotations
 def foo
 
 def foo() {
@@ -202,50 +172,20 @@ def foo() {
 """
 
     where:
-    individual | contained | containerWithList || expected
-    0          | -1        | false             || 0
-    1          | -1        | false             || 1
-    2          | -1        | false             || 2
-    0          | 0         | false             || 0
-    0          | 0         | true              || 0
-    1          | 0         | false             || 1
-    1          | 0         | true              || 1
-    0          | 1         | false             || 1
-    0          | 1         | true              || 1
-    1          | 1         | false             || 2
-    1          | 1         | true              || 2
-    0          | 2         | true              || 2
-    1          | 2         | true              || 3
-  }
-
-  /**
-   * Builds the annotation container {@code @FooContainer} with the given amount of
-   * {@code @Foo} annotations, either in a list or plain. An amount of {@code -1} means
-   * no container annotation at all. With list {@code false} only makes sense for counts
-   * less than {@code 2}, otherwise invalid syntax is produced.
-   *
-   * <p><b>Examples:</b>
-   * <table>
-   *   <th><td> contained </td><td> containerWithList </td><td> returnValue                   </td></th>
-   *   <tr><td> -1        </td><td> n/a               </td><td> ''                            </td></tr>
-   *   <tr><td> 0         </td><td> false             </td><td> '@FooContainer()'             </td></tr>
-   *   <tr><td> 0         </td><td> true              </td><td> '@FooContainer([])'           </td></tr>
-   *   <tr><td> 1         </td><td> false             </td><td> '@FooContainer(@Foo)'         </td></tr>
-   *   <tr><td> 1         </td><td> true              </td><td> '@FooContainer([@Foo])'       </td></tr>
-   *   <tr><td> 2         </td><td> true              </td><td> '@FooContainer([@Foo, @Foo])' </td></tr>
-   * </table>
-   *
-   * @param contained the amount of {@code @Foo} annotations to include
-   * @param containerWithList whether to wrap the annotations in a {@code List} or have a plain value
-   * @return the built container annotation
-   */
-  static buildContainer(contained, containerWithList) {
-    contained < 0 ? '' :
-      '@FooContainer(' +
-        (containerWithList ? '[' : '') +
-        (['@Foo'] * contained).join(', ') +
-        (containerWithList ? ']' : '') +
-        ')'
+    annotations                             || expected
+    ''                                      || 0
+    '@Foo'                                  || 1
+    '@Foo @Foo'                             || 2
+    '          @FooContainer()'             || 0
+    '          @FooContainer([])'           || 0
+    '@Foo      @FooContainer()'             || 1
+    '@Foo      @FooContainer([])'           || 1
+    '          @FooContainer(@Foo)'         || 1
+    '          @FooContainer([@Foo])'       || 1
+    '@Foo      @FooContainer(@Foo)'         || 2
+    '@Foo      @FooContainer([@Foo])'       || 2
+    '          @FooContainer([@Foo, @Foo])' || 2
+    '@Foo      @FooContainer([@Foo, @Foo])' || 3
   }
 
   static class FooExtension implements IAnnotationDrivenExtension<Foo> {

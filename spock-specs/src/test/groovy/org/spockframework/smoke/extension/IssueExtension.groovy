f3aefd76fdd602968cce41ab880251104c13b8e9
Björn Kautler <Bjoern@Kautler.net>
2020-07-14 16:30:41 +0200

    Make @Issue repeatable (#1189)
    Fixes #1030

--- spock-specs/src/test/groovy/org/spockframework/smoke/extension/IssueExtension.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/extension/IssueExtension.groovy
@@ -61,11 +61,65 @@ class IssueExtension extends EmbeddedSpecification {
       url == "http://my.issues.org/FOO-1"
     }
     with(tags[1]) {
-        name == "FOO-2"
-        key == "issue"
-        value == "FOO-2"
-        url == "http://my.issues.org/FOO-2"
-      }
+      name == "FOO-2"
+      key == "issue"
+      value == "FOO-2"
+      url == "http://my.issues.org/FOO-2"
+    }
+  }
+
+  @Issue("http://my.issues.org/FOO-1")
+  @Issue("http://my.issues.org/FOO-2")
+  def "if multiple issue annotations are applied, all of them are converted to a tag"() {
+    def tags = specificationContext.currentFeature.tags
+
+    expect:
+    tags.size() == 2
+    with(tags[0]) {
+      name == "FOO-1"
+      key == "issue"
+      value == "FOO-1"
+      url == "http://my.issues.org/FOO-1"
+    }
+    with(tags[1]) {
+      name == "FOO-2"
+      key == "issue"
+      value == "FOO-2"
+      url == "http://my.issues.org/FOO-2"
+    }
+  }
+
+  @Issue(["http://my.issues.org/FOO-1", "http://my.issues.org/FOO-2"])
+  @Issue(["http://my.issues.org/FOO-3", "http://my.issues.org/FOO-4"])
+  def "if multiple issue annotations with multiple issues are applied, all of them are converted to a tag"() {
+    def tags = specificationContext.currentFeature.tags
+
+    expect:
+    tags.size() == 4
+    with(tags[0]) {
+      name == "FOO-1"
+      key == "issue"
+      value == "FOO-1"
+      url == "http://my.issues.org/FOO-1"
+    }
+    with(tags[1]) {
+      name == "FOO-2"
+      key == "issue"
+      value == "FOO-2"
+      url == "http://my.issues.org/FOO-2"
+    }
+    with(tags[2]) {
+      name == "FOO-3"
+      key == "issue"
+      value == "FOO-3"
+      url == "http://my.issues.org/FOO-3"
+    }
+    with(tags[3]) {
+      name == "FOO-4"
+      key == "issue"
+      value == "FOO-4"
+      url == "http://my.issues.org/FOO-4"
+    }
   }
 
   def "complains if @Issue is used on anything other than a spec or feature"() {

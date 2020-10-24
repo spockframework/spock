7e8ab39e71dd9bd1165da092aac2ae568a437ce6
Leonard Brünings <lord_damokles@gmx.net>
2020-10-24 22:20:26 +0200

    Implement feedback from review

--- spock-core/src/main/java/org/spockframework/runtime/extension/builtin/IsolatedExtension.java
+++ spock-core/src/main/java/org/spockframework/runtime/extension/builtin/IsolatedExtension.java
@@ -2,9 +2,7 @@ package org.spockframework.runtime.extension.builtin;
 
 import org.spockframework.runtime.extension.IAnnotationDrivenExtension;
 import org.spockframework.runtime.model.SpecInfo;
-import org.spockframework.runtime.model.parallel.ExclusiveResource;
-import org.spockframework.runtime.model.parallel.ResourceAccessMode;
-
+import org.spockframework.runtime.model.parallel.*;
 import spock.lang.Isolated;
 
 import java.util.List;
@@ -12,7 +10,7 @@ import java.util.List;
 public class IsolatedExtension implements IAnnotationDrivenExtension<Isolated> {
 
   private static final ExclusiveResource GLOBAL_LOCK = new ExclusiveResource(
-    "org.junit.platform.engine.support.hierarchical.ExclusiveResource.GLOBAL_KEY", ResourceAccessMode.READ_WRITE);
+    org.junit.platform.engine.support.hierarchical.ExclusiveResource.GLOBAL_KEY, ResourceAccessMode.READ_WRITE);
 
   @Override
   public void visitSpecAnnotations(List<Isolated> annotations, SpecInfo spec) {

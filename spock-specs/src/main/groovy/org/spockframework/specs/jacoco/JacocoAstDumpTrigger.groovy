5d7001eceff1bc3f555e4f5c4116bffd87eb6f3d
Leonard Brünings <lord_damokles@gmx.net>
2021-03-14 15:43:31 +0100

    Add compiler coverage for Spock's AST transformations (#1278)

---
+++ spock-specs/src/main/groovy/org/spockframework/specs/jacoco/JacocoAstDumpTrigger.groovy
@@ -0,0 +1,26 @@
+package org.spockframework.specs.jacoco
+
+import java.lang.management.ManagementFactory
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.control.*
+import org.codehaus.groovy.transform.*
+
+/**
+ * This is a workaround until https://github.com/gradle/gradle/issues/16438 gets fixed.
+ */
+@CompileStatic
+@GroovyASTTransformation(phase = CompilePhase.OUTPUT)
+class JacocoAstDumpTrigger implements ASTTransformation {
+  private boolean dumped = false
+
+  @Override
+  void visit(ASTNode[] nodes, SourceUnit source) {
+    if(!dumped) {
+      dumped = true
+      new GroovyMBean(ManagementFactory.platformMBeanServer, "org.jacoco:type=Runtime")
+        .invokeMethod('dump', true)
+    }
+  }
+}

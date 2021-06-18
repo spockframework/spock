3bac268f0df3daafe9549603e362f1a9f55ea677
Leonard Brünings <lord_damokles@gmx.net>
2021-06-18 19:17:27 +0200

    Simplify PR

--- spock-testkit/src/test/groovy/spock/platform/InheritedTest.java
+++ spock-testkit/src/test/groovy/spock/platform/InheritedTest.java
@@ -1,58 +1,53 @@
 package spock.platform;
 
-import org.apache.maven.surefire.api.testset.*;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.engine.FilterResult;
-import org.junit.platform.engine.TestDescriptor;
-import org.junit.platform.engine.support.descriptor.MethodSource;
-import org.junit.platform.launcher.PostDiscoveryFilter;
-import org.junit.platform.testkit.engine.EventStatistics;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
 import spock.testkit.testsources.InheritedChildTestCase;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.regex.Pattern;
 
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.*;
+import org.junit.platform.engine.support.descriptor.MethodSource;
+import org.junit.platform.launcher.PostDiscoveryFilter;
+import org.junit.platform.testkit.engine.EventStatistics;
+
 class InheritedTest extends SpockEngineBase {
 
   @Test
   void verifyInheritedTests() {
     Consumer<EventStatistics> assertions = stats -> stats.started(3).succeeded(2).failed(1).skipped(1);
-    execute(selectClass(InheritedChildTestCase.class), assertions, Arrays.asList(new TestMethodFilter(new TestListResolver("InheritedChildTestCase"))));
+    execute(selectClass(InheritedChildTestCase.class), assertions, Arrays.asList(new TestMethodFilter(Pattern.compile(".*InheritedChildTestCase"))));
   }
 
   /**
-   * Copy of Maven Surefire Plugin's TestMethodFilter -- it is private so it cannot be imported.
+   * Copy relevant features of Maven Surefire Plugin's TestMethodFilter to recreate it here
    */
   class TestMethodFilter
-    implements PostDiscoveryFilter
-  {
+    implements PostDiscoveryFilter {
 
-    private final TestListResolver testListResolver;
+    private final Pattern classPattern;
 
-    TestMethodFilter( TestListResolver testListResolver )
-    {
-      this.testListResolver = testListResolver;
+    TestMethodFilter(Pattern classPattern) {
+      this.classPattern = classPattern;
     }
 
     @Override
-    public FilterResult apply(TestDescriptor descriptor )
-    {
+    public FilterResult apply(TestDescriptor descriptor) {
       boolean shouldRun = descriptor.getSource()
-        .filter( MethodSource.class::isInstance )
-        .map( MethodSource.class::cast )
-        .map( this::shouldRun )
-        .orElse( true );
+        .filter(MethodSource.class::isInstance)
+        .map(MethodSource.class::cast)
+        .map(this::shouldRun)
+        .orElse(true);
 
-      return FilterResult.includedIf( shouldRun );
+      return FilterResult.includedIf(shouldRun);
     }
 
-    private boolean shouldRun( MethodSource source )
-    {
-      String testClass = TestListResolver.toClassFileName( source.getClassName() );
-      String testMethod = source.getMethodName();
-      System.out.println("RESOLVING NAME " + testClass + "#" + testMethod);
-      return this.testListResolver.shouldRun( testClass, testMethod );
+    private boolean shouldRun(MethodSource source) {
+      String testClass = source.getClassName();
+      return this.classPattern.matcher(testClass).matches();
     }
   }
 

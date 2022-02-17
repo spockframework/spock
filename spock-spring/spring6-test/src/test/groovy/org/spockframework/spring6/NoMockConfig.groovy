c83595bc820973867430015535b9dc101f40b914
Leonard Brünings <leonard.bruenings@gradle.com>
2022-02-17 12:24:14 +0100

    Fix Spring 6 incompatibility (#1428)
    Add BackwardsCompatibleInstantiationAwareBeanPostProcessorAdapter to fix issues with Spring 6

---
+++ spock-spring/spring6-test/src/test/groovy/org/spockframework/spring6/NoMockConfig.groovy
@@ -0,0 +1,35 @@
+package org.spockframework.spring6
+
+import groovy.transform.CompileStatic
+import org.springframework.context.annotation.Bean
+
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+
+@CompileStatic
+class NoMockConfig {
+
+  @Bean
+  ExecutorService executor() {
+    throw new RuntimeException("This should not be called")
+  }
+
+  @Bean
+  ServiceExecutor serviceExecutor(ExecutorService executorService) {
+    return new ServiceExecutor(executorService)
+  }
+
+
+}
+
+class ServiceExecutor {
+  private final ExecutorService executorService
+
+  ServiceExecutor(ExecutorService executorService) {
+    this.executorService = executorService
+  }
+
+  def exec() {
+    executorService.submit({"done"} as Callable).get()
+  }
+}

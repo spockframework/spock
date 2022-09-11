c165dc628168e3539d1be49b6962cc717ff4160f
Leonard Brünings <lord_damokles@gmx.net>
2022-09-11 22:13:14 +0200

    Fix issue with Object methods on interface spies
    fixes #1516

---
+++ spock-core/src/main/java/org/spockframework/mock/runtime/ObjectMethodInvoker.java
@@ -0,0 +1,21 @@
+package org.spockframework.mock.runtime;
+
+import org.spockframework.mock.*;
+
+public class ObjectMethodInvoker implements IResponseGenerator {
+
+  public static final ObjectMethodInvoker INSTANCE = new ObjectMethodInvoker();
+
+  private ObjectMethodInvoker() {
+  }
+
+  @Override
+  public Object respond(IMockInvocation invocation) {
+    IMockInteraction interaction = DefaultJavaLangObjectInteractions.INSTANCE.match(invocation);
+    if (interaction != null) {
+      return interaction.accept(invocation);
+    }
+
+    return null;
+  }
+}

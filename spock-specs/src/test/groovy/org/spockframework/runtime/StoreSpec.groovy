5848504f18f1f9f446d9575b5777b880f883e007
Leonard Brünings <leonard.bruenings@gradle.com>
2022-10-27 19:04:32 +0200

    Add exhaustive cleanup check

--- spock-specs/src/test/groovy/org/spockframework/runtime/StoreSpec.groovy
+++ spock-specs/src/test/groovy/org/spockframework/runtime/StoreSpec.groovy
@@ -4,8 +4,11 @@ import org.spockframework.EmbeddedSpecification
 import org.spockframework.runtime.extension.*
 import org.spockframework.runtime.model.MethodKind
 import org.spockframework.runtime.model.SpecInfo
+import org.spockframework.runtime.model.parallel.ExecutionMode
 import org.spockframework.util.Assert
 import org.spockframework.util.InternalSpockError
+import spock.lang.Execution
+import spock.lang.Shared
 
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
@@ -19,8 +22,10 @@ class StoreSpec extends EmbeddedSpecification {
   }
 
   def "store is created and cleaned for each level"() {
+    given:
+    LoggingStoreInterceptor.HACKY_SHARED_LIST_OF_ACTIONS.clear()
+
     when:
-    println "------------TEST START----------------"
     runner.runWithImports('''
     @LogStoreUsage
     class ASpec extends Specification {
@@ -29,31 +34,96 @@ class StoreSpec extends EmbeddedSpecification {
       }
     }
 ''')
-    println "------------TEST END------------------"
 
     then:
-    noExceptionThrown()
+    LoggingStoreInterceptor.HACKY_SHARED_LIST_OF_ACTIONS == [
+      "before SHARED_INITIALIZER - null",
+      "	prev:     null",
+      "	replaced: null",
+      "	put:      Stored at SHARED_INITIALIZER - null",
+      "after SHARED_INITIALIZER - null",
+      "before SPEC_EXECUTION - null",
+      "	prev:     Stored at SHARED_INITIALIZER - null",
+      "	replaced: Stored at SHARED_INITIALIZER - null",
+      "	put:      Stored at SPEC_EXECUTION - null",
+      "before SETUP_SPEC - null",
+      "	prev:     Stored at SPEC_EXECUTION - null",
+      "	replaced: Stored at SPEC_EXECUTION - null",
+      "	put:      Stored at SETUP_SPEC - null",
+      "after SETUP_SPEC - null",
+      "before INITIALIZER - null",
+      "	prev:     Stored at SETUP_SPEC - null",
+      "	replaced: null",
+      "	put:      Stored at INITIALIZER - null",
+      "after INITIALIZER - null",
+      "before FEATURE_EXECUTION - null",
+      "	prev:     Stored at INITIALIZER - null",
+      "	replaced: Stored at INITIALIZER - null",
+      "	put:      Stored at FEATURE_EXECUTION - null",
+      "before ITERATION_EXECUTION - null",
+      "	prev:     Stored at FEATURE_EXECUTION - null",
+      "	replaced: Stored at FEATURE_EXECUTION - null",
+      "	put:      Stored at ITERATION_EXECUTION - null",
+      "before SETUP - null",
+      "	prev:     Stored at ITERATION_EXECUTION - null",
+      "	replaced: Stored at ITERATION_EXECUTION - null",
+      "	put:      Stored at SETUP - null",
+      "after SETUP - null",
+      "before FEATURE - a feature",
+      "	prev:     Stored at SETUP - null",
+      "	replaced: Stored at SETUP - null",
+      "	put:      Stored at FEATURE - a feature",
+      "after FEATURE - a feature",
+      "before CLEANUP - null",
+      "	prev:     Stored at FEATURE - a feature",
+      "	replaced: Stored at FEATURE - a feature",
+      "	put:      Stored at CLEANUP - null",
+      "after CLEANUP - null",
+      "# closing Stored at CLEANUP - null",
+      "# closing Stored at FEATURE - a feature",
+      "# closing Stored at SETUP - null",
+      "# closing Stored at ITERATION_EXECUTION - null",
+      "# closing Stored at FEATURE_EXECUTION - null",
+      "# closing Stored at INITIALIZER - null",
+      "after ITERATION_EXECUTION - null",
+      "after FEATURE_EXECUTION - null",
+      "before CLEANUP_SPEC - null",
+      "	prev:     Stored at SETUP_SPEC - null",
+      "	replaced: Stored at SETUP_SPEC - null",
+      "	put:      Stored at CLEANUP_SPEC - null",
+      "after CLEANUP_SPEC - null",
+      "# closing Stored at CLEANUP_SPEC - null",
+      "# closing Stored at SETUP_SPEC - null",
+      "# closing Stored at SPEC_EXECUTION - null",
+      "# closing Stored at SHARED_INITIALIZER - null",
+      "after SPEC_EXECUTION - null"
+    ]
   }
 
+  @Shared
+  def supportedMethodKinds = ((MethodKind.values() as List) - [MethodKind.DATA_PROCESSOR, MethodKind.DATA_PROVIDER])
+
+  @Execution(ExecutionMode.SAME_THREAD)
   def "store cleanup fails"() {
+    given:
+    FailingValue.HACKY_SHARED_LIST_OF_CLEANUPS.clear()
+
     when:
-    println "------------TEST START----------------"
     runner.runWithImports("""
-    @FailingStoreUsage($methodKind)
+    @FailingStoreUsage([${methodKind.join(', ')}])
     class ASpec extends Specification {
       def "a feature"() {
         expect: true
       }
     }
 """)
-    println "------------TEST END------------------"
-
     then:
     InternalSpockError e = thrown()
-    e.message.startsWith("Failing at $methodKind - ")
+    e.message.startsWith("Failing at ")
+    FailingValue.HACKY_SHARED_LIST_OF_CLEANUPS ==~ supportedMethodKinds
 
     where:
-    methodKind << (MethodKind.values() - [MethodKind.DATA_PROCESSOR, MethodKind.DATA_PROVIDER])
+    methodKind << supportedMethodKinds.subsequences()
   }
 }
 
@@ -90,18 +160,23 @@ class LoggingStoreInterceptor implements IMethodInterceptor {
   static final IStore.Namespace NAMESPACE = IStore.Namespace.create(LoggingStoreInterceptor.class)
   static final LoggingStoreInterceptor INSTANCE = new LoggingStoreInterceptor()
 
+  static List<MethodKind> HACKY_SHARED_LIST_OF_ACTIONS = []
+
   @Override
   void intercept(IMethodInvocation invocation) throws Throwable {
     def store = invocation.getStore(NAMESPACE)
-    def prev = store.get("message", LoggingValue)
-    def newValue = new LoggingValue("Stored at $invocation.method.kind - $invocation.method.name")
-    def replaced = store.put("message", newValue)
-    println "before $invocation.method.kind - $invocation.method.name"
-    println("\tprev:     ${prev?.value}")
-    println("\treplaced: ${replaced?.value}")
-    println("\tput:      ${newValue.value}")
+    String message = "Stored at $invocation.method.kind - $invocation.method.name"
+    def prev = store.get("message", String)
+    def replaced = store.put("message", message)
+
+    store.put(invocation.method.kind, new LoggingValue(message))
+
+    HACKY_SHARED_LIST_OF_ACTIONS << "before $invocation.method.kind - $invocation.method.name".toString()
+    HACKY_SHARED_LIST_OF_ACTIONS << "\tprev:     ${prev}".toString()
+    HACKY_SHARED_LIST_OF_ACTIONS << "\treplaced: ${replaced}".toString()
+    HACKY_SHARED_LIST_OF_ACTIONS << "\tput:      ${message}".toString()
     invocation.proceed()
-    println "after $invocation.method.kind - $invocation.method.name"
+    HACKY_SHARED_LIST_OF_ACTIONS << "after $invocation.method.kind - $invocation.method.name".toString()
   }
 }
 
@@ -115,7 +190,7 @@ class LoggingValue implements AutoCloseable {
 
   @Override
   void close() throws Exception {
-    println "# closing ${this.value}"
+    LoggingStoreInterceptor.HACKY_SHARED_LIST_OF_ACTIONS << "# closing ${this.value}".toString()
   }
 }
 
@@ -166,6 +241,7 @@ class FailingCleanupStoreInterceptor implements IMethodInterceptor {
     def store = invocation.getStore(NAMESPACE)
     store.put(invocation.method.kind,
       new FailingValue(
+        invocation.method.kind,
         "Failing at $invocation.method.kind - $invocation.method.name",
         annotation.value().contains(invocation.method.kind))
     )
@@ -175,16 +251,21 @@ class FailingCleanupStoreInterceptor implements IMethodInterceptor {
 
 class FailingValue implements AutoCloseable {
 
+  static List<MethodKind> HACKY_SHARED_LIST_OF_CLEANUPS = []
+
   final String value
   final boolean failAtCleanup
+  private final MethodKind methodKind
 
-  FailingValue(String value, boolean failAtCleanup) {
+  FailingValue(MethodKind methodKind, String value, boolean failAtCleanup) {
+    this.methodKind = methodKind
     this.failAtCleanup = failAtCleanup
     this.value = value
   }
 
   @Override
   void close() throws Exception {
+    HACKY_SHARED_LIST_OF_CLEANUPS << methodKind
     Assert.that(!failAtCleanup, value)
   }
 }

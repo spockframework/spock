edce7499d148babb25f7fa4db49339c18d2d78b3
Leonard Brünings <leonard.bruenings@gradle.com>
2023-02-16 19:16:41 +0100

    Update AST tests with block listeners

--- spock-specs/src/test/resources/snapshots/org/spockframework/smoke/ast/AstSpec/astToSourceFeatureBody_renders_only_methods_and_its_annotation_by_default.groovy
+++ spock-specs/src/test/resources/snapshots/org/spockframework/smoke/ast/AstSpec/astToSourceFeatureBody_renders_only_methods_and_its_annotation_by_default.groovy
@@ -6,8 +6,13 @@ class ASpec extends Specification {
 /*--------- tag::snapshot[] ---------*/
 @org.spockframework.runtime.model.FeatureMetadata(name = 'a feature', ordinal = 0, line = 1, blocks = [@org.spockframework.runtime.model.BlockMetadata(kind = org.spockframework.runtime.model.BlockKind.SETUP, texts = [])], parameterNames = [])
 public void $spock_feature_0_0() {
-    java.lang.Object nothing = null
-    this.getSpecificationContext().getMockController().leaveScope()
+    try {
+        org.spockframework.runtime.SpockRuntime.callEnterBlock(this.getSpecificationContext(), new org.spockframework.runtime.model.BlockInfo(org.spockframework.runtime.model.BlockKind.SETUP, []))
+        java.lang.Object nothing = null
+        this.getSpecificationContext().getMockController().leaveScope()
+    }
+    finally {
+        org.spockframework.runtime.SpockRuntime.clearCurrentBlock(this.getSpecificationContext())}
 }
 /*--------- end::snapshot[] ---------*/
   }

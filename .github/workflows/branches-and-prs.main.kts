46b3ccbb21d99e36986b84792ce7014234f20303
Björn Kautler <Bjoern@Kautler.net>
2023-04-11 17:26:51 +0200

    Improve Codecov settings
    Fail the CI build if the Codecov uploading failed.
    Show more detailed information in the Codecov comment.
    Do not update the Codecov comment 15 times, but post it after all 16 reports were uploaded.
    Ignore dates in coverage reports to not get "upload expired" because the report was taken from remote build cache.

--- .github/workflows/branches-and-prs.main.kts
+++ .github/workflows/branches-and-prs.main.kts
@@ -111,7 +111,9 @@ workflow(
         )
         uses(
             name = "Upload to Codecov.io",
-            action = CodecovAction()
+            action = CodecovAction(
+                failCiIfError = true
+            )
         )
     }
 }

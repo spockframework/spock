c878a33dfb04737a222d7c452d4b8bb65bacf15a
Björn Kautler <Bjoern@Kautler.net>
2023-04-28 00:03:06 +0200

    Add SpecInfo#getAllCleanupSpecMethods()

--- spock-specs/src/test/groovy/org/spockframework/smoke/Interceptors.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/Interceptors.groovy
@@ -196,7 +196,7 @@ class FooSpec extends Specification {
       specInfo.addCleanupSpecInterceptor {
         proceed(it, 'cleanup spec', "$it.spec.name")
       }
-      specInfo.specsBottomToTop.collectMany { it.cleanupSpecMethods }*.addInterceptor {
+      specInfo.allCleanupSpecMethods*.addInterceptor {
         proceed(it, 'cleanup spec method', "$it.spec.name.$it.method.name()")
       }
       specInfo.allFixtureMethods*.addInterceptor {

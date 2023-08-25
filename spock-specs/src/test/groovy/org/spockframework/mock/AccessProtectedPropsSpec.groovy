9c83fc429942d32f5d18fafc1a559a0fa4320d38
Andreas Turban <andreas.turban@net-baustelle.de>
2023-08-25 16:56:51 +0200

    Update spock-specs/src/test/groovy/org/spockframework/mock/AccessProtectedPropsSpec.groovy
    Co-authored-by: Björn Kautler <Bjoern@Kautler.net>

--- spock-specs/src/test/groovy/org/spockframework/mock/AccessProtectedPropsSpec.groovy
+++ spock-specs/src/test/groovy/org/spockframework/mock/AccessProtectedPropsSpec.groovy
@@ -42,7 +42,7 @@ class AccessProtectedPropsSpec extends Specification {
   @Issue("https://github.com/spockframework/spock/issues/1608")
   def "Access protected should be accessible in Groovy 3&4 Issue #1501"() {
     when:
-    AccessProtectedSubClass mySpy = Spy(AccessProtectedSubClass)
+    AccessProtectedSubClass mySpy = Spy()
     then:
     mySpy.accessNonStaticFlag()
   }

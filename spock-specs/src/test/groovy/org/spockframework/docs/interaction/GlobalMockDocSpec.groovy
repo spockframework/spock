36c984de037206aaecc24d0fe845fbdef7d83d0a
Andreas Turban <andreas.turban@net-baustelle.de>
2023-09-15 16:42:16 +0200

    Clarify Documentation for global Mocks (#1755)
    Added test case base documentation for global mocks. And clarify the usage of the global mock and lifecycle.
    
    This fixes #785

---
+++ spock-specs/src/test/groovy/org/spockframework/docs/interaction/GlobalMockDocSpec.groovy
@@ -0,0 +1,57 @@
+package org.spockframework.docs.interaction
+
+import spock.lang.Issue
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+@Stepwise
+class GlobalMockDocSpec extends Specification {
+
+  @Issue("https://github.com/spockframework/spock/issues/785")
+  def "Global Spy usage"() {
+    // tag::global-spy[]
+    given:
+    def publisher = new Publisher()
+    def anySubscriber = GroovySpy(global: true, RealSubscriber)
+    publisher.subscribers.add(new RealSubscriber())
+    publisher.subscribers.add(new RealSubscriber())
+
+    when:
+    publisher.send("message")
+
+    then:
+    2 * anySubscriber.receive("message")
+    // end::global-spy[]
+  }
+
+  def "Global GroovyMock mocks also constructor"() {
+    // tag::global-mock-constructor[]
+    given:
+    GroovyMock(global: true, RealSubscriber)
+    when:
+    def sub = new RealSubscriber()
+    then: "The GroovyMock(global: true) will also mock the constructor"
+    sub == null
+    // end::global-mock-constructor[]
+  }
+
+  def "Global GroovyMock but with real constructor"() {
+    // tag::global-mock-constructor-real[]
+    given:
+    GroovyMock(global: true, RealSubscriber) {
+      //Allow that the real constructor is called
+      new RealSubscriber(*_) >> { callRealMethod() }
+    }
+    when:
+    def sub = new RealSubscriber()
+    then:
+    sub instanceof RealSubscriber
+    // end::global-mock-constructor-real[]
+  }
+
+  static class RealSubscriber implements Subscriber {
+    @Override
+    void receive(String message) {
+    }
+  }
+}

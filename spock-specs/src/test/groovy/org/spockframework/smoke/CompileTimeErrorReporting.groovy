2c7db7772f65dc691dca9d1e5533783934a1aad1
Leonard Brünings <leonard.bruenings@gradle.com>
2024-01-25 19:13:36 +0100

    Remove leftover println

--- spock-specs/src/test/groovy/org/spockframework/smoke/CompileTimeErrorReporting.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/CompileTimeErrorReporting.groovy
@@ -1,15 +1,16 @@
 /*
- * Copyright 2009 the original author or authors.
+ * Copyright 2024 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     https://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 
 package org.spockframework.smoke
@@ -98,7 +99,6 @@ println "hi"
 
     then: 'make sure it would fail for the right reason, this might change if `Debug` makes it into the runtime classpath'
     NoClassDefFoundError ncdfe = thrown()
-    println(ncdfe.message)
     ncdfe.message.contains('org.spockframework.smoke.CompileTimeErrorReporting$Foo')
     ncdfe.message.contains('org/jetbrains/annotations/Debug')
 

225e84a34df295da4c1f3c7b2af8512dd10feba4
Leonard Brünings <leonard.bruenings@gradle.com>
2024-02-16 11:51:41 +0100

    Add ResourceLock to VersionCheckerSpec

--- spock-specs/src/test/groovy/org/spockframework/util/VersionCheckerSpec.groovy
+++ spock-specs/src/test/groovy/org/spockframework/util/VersionCheckerSpec.groovy
@@ -1,20 +1,24 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2024 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     https://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 package org.spockframework.util
 
-import org.spockframework.runtime.model.parallel.*
-import spock.lang.*
+
+import org.spockframework.runtime.model.parallel.ResourceAccessMode
+import org.spockframework.runtime.model.parallel.Resources
+import spock.lang.ResourceLock
+import spock.lang.Specification
 import spock.util.environment.RestoreSystemProperties
 
 class VersionCheckerSpec extends Specification {
@@ -44,6 +48,7 @@ class VersionCheckerSpec extends Specification {
     thrown(IncompatibleGroovyVersionException)
   }
 
+  @ResourceLock(Resources.SYSTEM_ERR)
   @RestoreSystemProperties
   void "suppress throwing exception on incompatible Groovy version if check is disabled"() {
     given:

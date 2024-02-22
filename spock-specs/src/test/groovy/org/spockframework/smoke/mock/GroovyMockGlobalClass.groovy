269bbedd5915ab68a6fe1b3f0e7e5a7131e8a67c
Leonard Brünings <leonard.bruenings@gradle.com>
2024-02-22 22:45:10 +0100

    Add optional reason to `@ResourceLock`

--- spock-specs/src/test/groovy/org/spockframework/smoke/mock/GroovyMockGlobalClass.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/mock/GroovyMockGlobalClass.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.spockframework.smoke.mock
 
 import org.spockframework.runtime.model.parallel.Resources
@@ -5,7 +20,10 @@ import spock.lang.Issue
 import spock.lang.ResourceLock
 import spock.lang.Specification
 
-@ResourceLock(Resources.META_CLASS_REGISTRY)
+@ResourceLock(
+  value = Resources.META_CLASS_REGISTRY,
+  reason = "Global Mocks"
+)
 @Issue('https://github.com/spockframework/spock/issues/761')
 class GroovyMockGlobalClass extends Specification {
 

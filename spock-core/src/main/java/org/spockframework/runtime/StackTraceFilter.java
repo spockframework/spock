7e1cf5b51cdb204142e46773a03ff63d9b7180aa
Leonard Brünings <lbruenings@gradle.com>
2024-02-25 09:16:49 +0100

    Add `java.lang.invoke.*` to StackTraceFilter (#1892)

--- spock-core/src/main/java/org/spockframework/runtime/StackTraceFilter.java
+++ spock-core/src/main/java/org/spockframework/runtime/StackTraceFilter.java
@@ -1,17 +1,16 @@
 /*
- * Copyright 2009 the original author or authors.
+ * Copyright 2024 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package org.spockframework.runtime;
@@ -38,6 +37,7 @@ public class StackTraceFilter implements IStackTraceFilter {
       "|\\Qorg.codehaus.groovy.vmplugin.v8.\\E.*" +
       "|groovy\\..*MetaClass.*" +
       "|groovy\\.lang\\.MetaMethod" +
+      "|\\Qjava.lang.invoke.\\E.*" +
       "|\\Qjava.lang.reflect.\\E.*" +
       "|sun\\.reflect\\..*" +
       "|\\Qjdk.internal.reflect.\\E.*" +
--- spock-specs/src/test/groovy/org/spockframework/smoke/StackTraceFiltering.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/StackTraceFiltering.groovy
@@ -1,17 +1,16 @@
 /*
- * Copyright 2008 the original author or authors.
+ * Copyright 2024 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package org.spockframework.smoke
@@ -324,6 +323,7 @@ apackage.ASpec|a feature|2
       new StackTraceElement('groovy.FooMetaClassBaz', 'bar', null, 0),
       new StackTraceElement('groovy.lang.MetaMethod', 'bar', null, 0),
       new StackTraceElement('java.lang.reflect.Foo', 'bar', null, 0),
+      new StackTraceElement('java.lang.invoke.LambdaForm', 'invoke', null, 0),
       new StackTraceElement('sun.reflect.Foo', 'bar', null, 0),
       new StackTraceElement('jdk.internal.reflect.Foo', 'bar', null, 0),
       new StackTraceElement('org.spockframework.runtime.Foo', 'bar', null, 0),
@@ -349,6 +349,7 @@ org.spockframework.runtime.foo.Baz|bar|0
         new StackTraceElement('groovy.FooMetaClassBaz', 'bar', null, 0),
         new StackTraceElement('groovy.lang.MetaMethod', 'bar', null, 0),
         new StackTraceElement('java.lang.reflect.Foo', 'bar', null, 0),
+        new StackTraceElement('java.lang.invoke.LambdaForm', 'invoke', null, 0),
         new StackTraceElement('sun.reflect.Foo', 'bar', null, 0),
         new StackTraceElement('jdk.internal.reflect.Foo', 'bar', null, 0),
         new StackTraceElement('org.spockframework.runtime.Foo', 'bar', null, 0),

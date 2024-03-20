1b4349e2efc575b33823d88a74d174bd001d0fe8
Leonard Brünings <leonard.bruenings@gradle.com>
2024-03-20 14:53:53 +0100

    Modernize getOrCreateExtension

--- spock-core/src/main/java/org/spockframework/runtime/ExtensionRunner.java
+++ spock-core/src/main/java/org/spockframework/runtime/ExtensionRunner.java
@@ -1,20 +1,24 @@
 /*
- * Copyright 2010 the original author or authors.
+ * Copyright 2024 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     https://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 
 package org.spockframework.runtime;
 
-import org.spockframework.runtime.extension.*;
+import org.spockframework.runtime.extension.ExtensionAnnotation;
+import org.spockframework.runtime.extension.IAnnotationDrivenExtension;
+import org.spockframework.runtime.extension.IGlobalExtension;
+import org.spockframework.runtime.extension.RepeatedExtensionAnnotations;
 import org.spockframework.runtime.model.*;
 import org.spockframework.util.Nullable;
 import org.spockframework.util.Pair;
@@ -160,11 +164,6 @@ public class ExtensionRunner {
 
 
   private IAnnotationDrivenExtension getOrCreateExtension(Class<? extends IAnnotationDrivenExtension> clazz) {
-    IAnnotationDrivenExtension extension = localExtensions.get(clazz);
-    if (extension == null) {
-      extension = configurationRegistry.instantiateExtension(clazz);
-      localExtensions.put(clazz, extension);
-    }
-    return extension;
+    return localExtensions.computeIfAbsent(clazz, configurationRegistry::instantiateExtension);
   }
 }

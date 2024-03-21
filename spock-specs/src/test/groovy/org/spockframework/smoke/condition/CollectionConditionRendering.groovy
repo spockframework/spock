78fc7b13fe539939c3f482b625855f32f4fb3a7e
Leonard Brünings <leonard.bruenings@gradle.com>
2024-03-21 15:37:11 +0100

    Add test case and reference issue

--- spock-specs/src/test/groovy/org/spockframework/smoke/condition/CollectionConditionRendering.groovy
+++ spock-specs/src/test/groovy/org/spockframework/smoke/condition/CollectionConditionRendering.groovy
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.spockframework.smoke.condition
+
+import spock.lang.Issue
 
 class CollectionConditionRendering extends ConditionRenderingSpec {
   def "nested lenient matching"() {
@@ -17,6 +34,7 @@ false
     }
   }
 
+  @Issue("https://github.com/spockframework/spock/issues/1930")
   def "nested regex finding"() {
     expect:
     isRendered """
@@ -29,6 +47,28 @@ false
       def x = 'foo'
       def y = /./
       assert (x =~ y).count == 0
+    }
+  }
+
+  @Issue("https://github.com/spockframework/spock/issues/1930")
+  def "nested regex complex finding"() {
+    expect:
+    isRendered """
+(output =~ /on (executor-\\d+)/).collect { it[1] }.unique().size() == 3
+ |      |                       |                 |        |      |
+ |      |                       |                 |        2      false
+ |      |                       |                 [executor-1, executor-2]
+ |      |                       [executor-1, executor-2]
+ |      java.util.regex.Matcher[pattern=on (executor-\\d+) region=0,54 lastmatch=]
+ Foo on executor-1
+ Bar on executor-2
+ Baz on executor-1""", {
+      def output = '''\
+Foo on executor-1
+Bar on executor-2
+Baz on executor-1
+'''
+      assert (output =~ /on (executor-\d+)/).collect { it[1] }.unique().size() == 3
     }
   }
 

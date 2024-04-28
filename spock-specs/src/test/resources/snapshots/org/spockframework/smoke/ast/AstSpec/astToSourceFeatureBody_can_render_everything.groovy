4eb7cf8b3dc2dfed69464faabc55af905916a78d
Leonard Brünings <leonard.bruenings@gradle.com>
2024-04-28 13:22:52 +0200

    Move current block clearing to PlatformSpecRunner

--- spock-specs/src/test/resources/snapshots/org/spockframework/smoke/ast/AstSpec/astToSourceFeatureBody_can_render_everything.groovy
+++ spock-specs/src/test/resources/snapshots/org/spockframework/smoke/ast/AstSpec/astToSourceFeatureBody_can_render_everything.groovy
@@ -7,14 +7,10 @@ public class apackage.ASpec extends spock.lang.Specification {
 
     @org.spockframework.runtime.model.FeatureMetadata(name = 'a feature', ordinal = 0, line = 1, blocks = [@org.spockframework.runtime.model.BlockMetadata(kind = org.spockframework.runtime.model.BlockKind.SETUP, texts = [])], parameterNames = [])
     public void $spock_feature_0_0() {
-        try {
-            org.spockframework.runtime.SpockRuntime.callEnterBlock(this.getSpecificationContext(), new org.spockframework.runtime.model.BlockInfo(org.spockframework.runtime.model.BlockKind.SETUP, []))
-            java.lang.Object nothing = null
-            org.spockframework.runtime.SpockRuntime.callExitBlock(this.getSpecificationContext(), new org.spockframework.runtime.model.BlockInfo(org.spockframework.runtime.model.BlockKind.SETUP, []))
-            this.getSpecificationContext().getMockController().leaveScope()
-        }
-        finally {
-            org.spockframework.runtime.SpockRuntime.clearCurrentBlock(this.getSpecificationContext())}
+        org.spockframework.runtime.SpockRuntime.callEnterBlock(this.getSpecificationContext(), new org.spockframework.runtime.model.BlockInfo(org.spockframework.runtime.model.BlockKind.SETUP, []))
+        java.lang.Object nothing = null
+        org.spockframework.runtime.SpockRuntime.callExitBlock(this.getSpecificationContext(), new org.spockframework.runtime.model.BlockInfo(org.spockframework.runtime.model.BlockKind.SETUP, []))
+        this.getSpecificationContext().getMockController().leaveScope()
     }
 
 }

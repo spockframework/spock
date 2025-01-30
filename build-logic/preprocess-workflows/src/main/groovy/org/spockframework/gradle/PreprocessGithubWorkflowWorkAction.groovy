cb7f0ed9c894864d9c47fe95791fa7d3fbb4a063
Björn Kautler <Bjoern@Kautler.net>
2025-01-30 19:28:07 +0100

    Add race-condition work-around

--- build-logic/preprocess-workflows/src/main/groovy/org/spockframework/gradle/PreprocessGithubWorkflowWorkAction.groovy
+++ build-logic/preprocess-workflows/src/main/groovy/org/spockframework/gradle/PreprocessGithubWorkflowWorkAction.groovy
@@ -16,17 +16,27 @@ abstract class PreprocessGithubWorkflowWorkAction implements WorkAction<Paramete
 
   @Override
   void execute() {
-    execOperations.javaexec {
-      it.executable = parameters.javaExecutable.get().asFile.absolutePath
-      it.classpath(parameters.kotlinCompilerClasspath)
-      it.mainClass.set('org.jetbrains.kotlin.cli.jvm.K2JVMCompiler')
-      it.args('-no-stdlib', '-no-reflect')
-      it.args('-classpath', parameters.mainKtsClasspath.asPath)
-      it.args('-script', parameters.workflowScript.get().asFile.absolutePath)
+    // work-around for https://youtrack.jetbrains.com/issue/KT-74830
+    Exception lastException = null
+    for (i in 1..5) {
+      try {
+        execOperations.javaexec {
+          it.executable = parameters.javaExecutable.get().asFile.absolutePath
+          it.classpath(parameters.kotlinCompilerClasspath)
+          it.mainClass.set('org.jetbrains.kotlin.cli.jvm.K2JVMCompiler')
+          it.args('-no-stdlib', '-no-reflect')
+          it.args('-classpath', parameters.mainKtsClasspath.asPath)
+          it.args('-script', parameters.workflowScript.get().asFile.absolutePath)
 
-      // work-around for https://youtrack.jetbrains.com/issue/KT-42101
-      it.systemProperty('kotlin.main.kts.compiled.scripts.cache.dir', '')
+          // work-around for https://youtrack.jetbrains.com/issue/KT-42101
+          it.systemProperty('kotlin.main.kts.compiled.scripts.cache.dir', '')
+        }
+        return
+      } catch (Exception e) {
+        lastException = e
+      }
     }
+    throw lastException
   }
 
   static interface Parameters extends WorkParameters {

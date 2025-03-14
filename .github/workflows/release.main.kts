6edf86cd38d54dd8c7e22625eb6c894ad11ea2b1
Björn Kautler <Bjoern@Kautler.net>
2025-03-14 18:35:20 +0100

    Use version ranges for actions to not use stale cache entries

--- .github/workflows/release.main.kts
+++ .github/workflows/release.main.kts
@@ -23,8 +23,8 @@
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.3.0")
 
 @file:Repository("https://bindings.krzeminski.it/")
-@file:DependsOn("actions:checkout:v4")
-@file:DependsOn("codecov:codecov-action:v5")
+@file:DependsOn("actions:checkout___major:[v4,v5-alpha)")
+@file:DependsOn("codecov:codecov-action___major:[v5,v6-alpha)")
 
 import io.github.typesafegithub.workflows.actions.actions.Checkout
 import io.github.typesafegithub.workflows.actions.actions.Checkout.FetchDepth

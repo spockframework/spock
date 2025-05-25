a88b966bed0f9073f6e32d2f4fd0ca186ec54388
Björn Kautler <Bjoern@Kautler.net>
2025-05-25 16:08:02 +0200

    Fix handling of condition method calls (#2162)

---
+++ spock-specs/src/test/resources/snapshots/org/spockframework/smoke/ast/condition/ConditionMethodsAstSpec/condition_method__conditionMethod_within_condition_method__conditionMethod-[1].groovy
@@ -0,0 +1,35 @@
+@org.spockframework.runtime.model.FeatureMetadata(name = 'a feature', ordinal = 0, line = 1, blocks = [@org.spockframework.runtime.model.BlockMetadata(kind = org.spockframework.runtime.model.BlockKind.EXPECT, texts = [])], parameterNames = [])
+public void $spock_feature_0_0() {
+    org.spockframework.runtime.SpockRuntime.callBlockEntered(this, 0)
+    this.verifyAll([''], { ->
+        org.spockframework.runtime.ValueRecorder $spock_valueRecorder1 = new org.spockframework.runtime.ValueRecorder()
+        org.spockframework.runtime.ErrorCollector $spock_errorCollector1 = new org.spockframework.runtime.ErrorCollector()
+        try {
+            try {
+                this.verifyAll([''], { ->
+                    org.spockframework.runtime.ValueRecorder $spock_valueRecorder2 = new org.spockframework.runtime.ValueRecorder()
+                    org.spockframework.runtime.ErrorCollector $spock_errorCollector2 = new org.spockframework.runtime.ErrorCollector()
+                    try {
+                        try {
+                            org.spockframework.runtime.SpockRuntime.verifyCondition($spock_errorCollector2, $spock_valueRecorder2.reset(), 'false', 4, 5, null, $spock_valueRecorder2.record($spock_valueRecorder2.startRecordingValue(0), false))
+                        }
+                        catch (java.lang.Throwable $spock_condition_throwable) {
+                            org.spockframework.runtime.SpockRuntime.conditionFailedWithException($spock_errorCollector2, $spock_valueRecorder2, 'false', 4, 5, null, $spock_condition_throwable)}
+                        finally {
+                        }
+                    }
+                    finally {
+                        $spock_errorCollector2.validateCollectedErrors()}
+                })
+            }
+            catch (java.lang.Throwable $spock_condition_throwable) {
+                org.spockframework.runtime.SpockRuntime.groupConditionFailedWithException($spock_errorCollector1, $spock_condition_throwable)}
+            finally {
+            }
+        }
+        finally {
+            $spock_errorCollector1.validateCollectedErrors()}
+    })
+    org.spockframework.runtime.SpockRuntime.callBlockExited(this, 0)
+    this.getSpecificationContext().getMockController().leaveScope()
+}
---
+++ spock-specs/src/test/resources/snapshots/org/spockframework/smoke/ast/condition/ConditionMethodsAstSpec/condition_method__conditionMethod_within_condition_method__conditionMethod_with_exception-[2].groovy
@@ -0,0 +1,27 @@
+@org.spockframework.runtime.model.FeatureMetadata(name = 'a feature', ordinal = 0, line = 1, blocks = [@org.spockframework.runtime.model.BlockMetadata(kind = org.spockframework.runtime.model.BlockKind.EXPECT, texts = [])], parameterNames = [])
+public void $spock_feature_0_0() {
+    org.spockframework.runtime.ErrorCollector $spock_errorCollector = org.spockframework.runtime.ErrorRethrower.INSTANCE
+    org.spockframework.runtime.SpockRuntime.callBlockEntered(this, 0)
+    this.verifyEach([''], { ->
+        org.spockframework.runtime.ValueRecorder $spock_valueRecorder1 = new org.spockframework.runtime.ValueRecorder()
+        try {
+            this.verifyEach([''], { ->
+                org.spockframework.runtime.ValueRecorder $spock_valueRecorder2 = new org.spockframework.runtime.ValueRecorder()
+                try {
+                    org.spockframework.runtime.SpockRuntime.verifyCondition($spock_errorCollector, $spock_valueRecorder2.reset(), 'true', 4, 5, null, $spock_valueRecorder2.record($spock_valueRecorder2.startRecordingValue(0), true))
+                }
+                catch (java.lang.Throwable $spock_condition_throwable) {
+                    org.spockframework.runtime.SpockRuntime.conditionFailedWithException($spock_errorCollector, $spock_valueRecorder2, 'true', 4, 5, null, $spock_condition_throwable)}
+                finally {
+                }
+                throw new java.lang.Exception('foo')
+            })
+        }
+        catch (java.lang.Throwable $spock_condition_throwable) {
+            org.spockframework.runtime.SpockRuntime.groupConditionFailedWithException($spock_errorCollector, $spock_condition_throwable)}
+        finally {
+        }
+    })
+    org.spockframework.runtime.SpockRuntime.callBlockExited(this, 0)
+    this.getSpecificationContext().getMockController().leaveScope()
+}

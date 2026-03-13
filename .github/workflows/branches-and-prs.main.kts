6a1c373910051d94678726dcf0a5fd3684e9d817
Björn Kautler <Bjoern@Kautler.net>
2026-03-13 14:37:10 +0100

    Use consistent versions for the checkout action

--- .github/workflows/branches-and-prs.main.kts
+++ .github/workflows/branches-and-prs.main.kts
@@ -40,6 +40,8 @@ import io.github.typesafegithub.workflows.domain.triggers.Push
 import io.github.typesafegithub.workflows.dsl.expressions.Contexts.github
 import io.github.typesafegithub.workflows.dsl.expressions.expr
 import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.CheckoutActionVersionSource.InferFromClasspath
+import io.github.typesafegithub.workflows.yaml.DEFAULT_CONSISTENCY_CHECK_JOB_CONFIG
 
 workflow(
     name = "Verify Branches and PRs",
@@ -58,6 +60,9 @@ workflow(
     concurrency = Concurrency(
         group = "${expr { github.workflow }}-${expr("${github.eventPullRequest.pull_request.number} || ${github.ref}")}",
         cancelInProgress = true
+    ),
+    consistencyCheckJobConfig = DEFAULT_CONSISTENCY_CHECK_JOB_CONFIG.copy(
+        checkoutActionVersion = InferFromClasspath()
     )
 ) {
     job(
